37847d738a863c85a4dcf2f258e2b3dfd4ac99dc
ElgarL <ElgarL@palmergames.com>
2012-02-02 21:19:15 +0000

    add missing NameValidation.java

---
+++ src/com/palmergames/bukkit/util/NameValidation.java
@@ -0,0 +1,118 @@
+/**
+ * 
+ */
+package com.palmergames.bukkit.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import javax.naming.InvalidNameException;
+
+import com.palmergames.bukkit.towny.TownySettings;
+
+/**
+ * @author ElgarL
+ *
+ */
+public class NameValidation {
+	
+	private static Pattern namePattern = null;
+	
+	/**
+	 *  Check and perform getNameCheckRegex on any town/nation names
+	 *  
+	 * @param name
+	 * @return result of getNameCheckRegex
+	 * @throws InvalidNameException
+	 */
+	public static String checkAndFilterName(String name) throws InvalidNameException {
+		String out = filterName(name);
+		
+		if (isBlacklistName(out))
+			throw new InvalidNameException(out + " is an invalid name.");
+
+		return out;
+	}
+	
+	/**
+	 * Check and perform regex on any Player names
+	 * 
+	 * @param name
+	 * @return
+	 * @throws InvalidNameException
+	 */
+	public static String checkAndFilterPlayerName(String name) throws InvalidNameException {
+		String out = filterName(name);
+		
+		if (!isValidName(out))
+			throw new InvalidNameException(out + " is an invalid name.");
+
+		return out;
+	}
+
+	/**
+	 * Perform regex on all names passed and return the results.
+	 * 
+	 * @param arr
+	 * @return
+	 */
+	public static String[] checkAndFilterArray(String[] arr) {
+		String[] out = arr;
+		int count = 0;
+
+		for (String word : arr) {
+			out[count] = filterName(word);
+			count++;
+		}
+
+		return out;
+	}
+	
+	/**
+	 * Is this name in our blacklist?
+	 * If not a blacklist, call isValidName and
+	 * return true if it is an invalid name.
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public static boolean isBlacklistName(String name) {
+    	// Max name length
+        if (name.length() > TownySettings.getMaxNameLength())
+        	return true;
+        /**
+         * A list of all banned names (notably all sub commands like 'spawn' used in '/town spawn')
+         */
+        ArrayList<String> bannedNames = new ArrayList<String>();
+        bannedNames.addAll(Arrays.asList("list","new","here","help","?","leave","withdraw","deposit","set","toggle","mayor","assistant","kick","add","claim","unclaim","title","outpost"));
+        // Banned names
+        if (bannedNames.contains(name.toLowerCase()))
+        	return true;
+        
+        return !isValidName(name);
+	}
+	
+	/**
+	 * Is this a valid name via getNameCheckRegex
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public static boolean isValidName(String name) {
+		try {
+            if (namePattern == null)
+                    namePattern = Pattern.compile(TownySettings.getNameCheckRegex());
+            return namePattern.matcher(name).find();
+		} catch (PatternSyntaxException e) {
+            e.printStackTrace();
+            return false;
+		}
+	}
+	
+	public static String filterName(String input) {
+		return input.replaceAll(TownySettings.getNameFilterRegex(), "_").replaceAll(TownySettings.getNameRemoveRegex(), "");
+	}
+
+}
