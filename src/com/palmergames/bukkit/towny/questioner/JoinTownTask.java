df792e17be38032b8caca2c7985930527f02fa9f
ElgarL <ElgarL@palmergames.com>
2012-04-30 15:10:34 +0100

    Example of how to add more observable types.

--- src/com/palmergames/bukkit/towny/questioner/JoinTownTask.java
+++ src/com/palmergames/bukkit/towny/questioner/JoinTownTask.java
@@ -1,5 +1,7 @@
 package com.palmergames.bukkit.towny.questioner;
 
+import static com.palmergames.bukkit.towny.object.TownyObservableType.TOWN_ADD_RESIDENT;
+
 import com.palmergames.bukkit.towny.TownyMessaging;
 import com.palmergames.bukkit.towny.TownySettings;
 import com.palmergames.bukkit.towny.exceptions.AlreadyRegisteredException;
@@ -7,6 +9,7 @@ import com.palmergames.bukkit.towny.exceptions.TownyException;
 import com.palmergames.bukkit.towny.object.Resident;
 import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.TownyUniverse;
+
 import com.palmergames.bukkit.util.ChatTools;
 
 public class JoinTownTask extends ResidentTownQuestionTask {
@@ -26,6 +29,9 @@ public class JoinTownTask extends ResidentTownQuestionTask {
 			TownyUniverse.getDataSource().saveTown(town);
 
 			TownyMessaging.sendTownMessage(town, ChatTools.color(String.format(TownySettings.getLangString("msg_join_town"), resident.getName())));
+			
+			universe.setChangedNotify(TOWN_ADD_RESIDENT);
+			
 		} catch (AlreadyRegisteredException e) {
 			try {
 				TownyMessaging.sendResidentMessage(resident, e.getMessage());
