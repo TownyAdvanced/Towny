0e4e85771cdcea0ac15064370d7936c64085dfd1
ElgarL <ElgarL@ElgarL-PC>
2014-03-22 23:13:08 +0000

    v0.84.0.12: 	- Fix a bug where you could create new towns in unclaimable worlds.

--- src/com/palmergames/bukkit/towny/command/TownCommand.java
+++ src/com/palmergames/bukkit/towny/command/TownCommand.java
@@ -1043,8 +1043,12 @@ public class TownCommand implements CommandExecutor {
 
 			if (!world.isUsingTowny())
 				throw new TownyException(TownySettings.getLangString("msg_set_use_towny_off"));
-
+			
+			if (world.isClaimable())
+				throw new TownyException(TownySettings.getLangString("msg_not_claimable"));
+			
 			Coord key = Coord.parseCoord(player);
+			
 			if (world.hasTownBlock(key))
 				throw new TownyException(String.format(TownySettings.getLangString("msg_already_claimed_1"), key));
 
