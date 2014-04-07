c151255e58e7e4cd38ce7709be89bbc4bc999250
ElgarL <ElgarL@ElgarL-PC>
2014-04-07 10:11:44 +0100

    v0.85.0.12: 	- Plot names can now be set and display for Town owned plots.

--- src/com/palmergames/bukkit/towny/ChunkNotification.java
+++ src/com/palmergames/bukkit/towny/ChunkNotification.java
@@ -176,7 +176,7 @@ public class ChunkNotification {
 
 	public String getOwnerNotification() {
 			
-		if (fromResident != toResident && !toWild) {
+		if (((fromResident != toResident) || (!fromTownBlock.getName().equalsIgnoreCase(toTownBlock.getName()))) && !toWild) {
 			if (toResident != null)
 				return String.format(ownerNotificationFormat, (toTownBlock.getName().isEmpty()) ? TownyFormatter.getFormattedName(toResident) : toTownBlock.getName());
 			else
