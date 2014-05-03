ac4988393f3923f399c92844cbdafc7b15c603d8
Neder <ocw5902@naver.com>
2014-05-03 20:12:07 +0900

    test

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-ftp -n -v ocw5902.hosting.paran.com << EOT
-ascii
+ftp -inv ocw5902.hosting.paran.com << EOT
 user ocw5902 6465902o
 prompt
 lcd /home/travis/build/Neder/Towny/out/
 cd public_html/Towny
-mput Towny*
+put Towny_CP949_$name.jar
+put Towny_UTF-8_$name.jar
 bye
 bye
 EOT
