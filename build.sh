45f4207ea22f74b0ac26cc5e56880ec708de1af6
Neder <ocw5902@naver.com>
2014-05-03 20:47:00 +0900

    Fucking TEST

--- build.sh
+++ build.sh
@@ -2,14 +2,7 @@ ls -l
 name=$(date +%Y%m%d)
 ftp -inv neder1.cloudapp.net << EOT
 user Towny \n
-pwd
-lcd /
-lcd home
-lcd travis
-lcd build
-lcd Neder
-lcd Towny
-lcd out
+help
 put Towny_C*
 put Towny_U*
 bye
