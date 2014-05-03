9894c6b49120fdccb52858905d8d9dbd2280448c
Neder <ocw5902@naver.com>
2014-05-03 20:27:55 +0900

    Server Change

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
-ftp -inv ocw5902.hosting.paran.com << EOT
-user ocw5902 6465902o
+ftp -inv neder1.cloudapp.net << EOT
+user Towny \n
 prompt
-lcd /home/travis/build/Neder/Towny/out/
-cd public_html/Towny
-put Towny_CP949_$name.jar
-put Towny_UTF-8_$name.jar
+lcd /home/travis/build/Neder/Towny/out
+put Towny_CP949_${name}.jar
+put Towny_UTF-8_${name}.jar
 bye
 bye
 EOT
