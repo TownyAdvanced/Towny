729bfd5714ce2f41962efcd1fda19a4a9a43b06c
Neder <ocw5902@naver.com>
2014-05-03 19:49:05 +0900

    test

--- build.sh
+++ build.sh
@@ -1,23 +1,3 @@
-name=$(date +%Y%H%M)
-
-cd /home/travis/build/Neder/Towny
-
-ant jar
-
-mv ./out/Towny.jar ./out/Towny_UTF-8_$name.jar
-
-sleep 10
-
-iconv -f UTF-8 -t cp949 ./src/korean.yml > koreancp949.yml
-rm ./src/korean.yml
-mv ./src/koreancp949.yml ./src/korean.yml
-rm ./src/com/palmergames/util/FileMgmt.java
-mv ./src/com/palmergames/util/FileMgmt.java.cp949 ./src/com/palmergames/util/FileMgmt.java
-
-ant jar
-
-mv ./out/Towny.jar ./out/Towny_CP949_$name.jar
-
 HOST='ocw5902.hosting.paran.com'
 USER='ocw5902'
 PASSWD='6465902o'
