e0ed504452e020ea1a3b9fb4c7cf4dfc33a00f77
Neder <ocw5902@naver.com>
2014-05-03 20:44:27 +0900

    test

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@ ls -l
 name=$(date +%Y%m%d)
 ftp -inv neder1.cloudapp.net << EOT
 user Towny \n
+pwd
+lcd /
+lcd home
+lcd travis
+lcd build
+lcd Neder
+lcd Towny
 lcd out
 put Towny_CP949_${name}.jar
 put Towny_UTF-8_${name}.jar
