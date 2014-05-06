4be709cb9f5d70b1b5122b1763dcdc19be5cb160
Neder <ocw5902@naver.com>
2014-05-06 13:28:17 +0900

    directory

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
+cd /home/travis/build/Neder/Towny
 echo "Pull Request Number(false = Master Build) => ${TRAVIS_PULL_REQUEST}"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     sudo curl -T ./out/Towny_${name}_CP949_${TRAVIS_COMMIT}.jar -u u833935363:TownyTowny ftp://ftp.ocw5902.w.pw/Towny/
