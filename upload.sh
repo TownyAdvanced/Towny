fff5ff6ac9ebebe9b21712d60896230cd43f3c65
Neder <ocw5902@naver.com>
2014-05-06 13:36:04 +0900

    date format change

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-cd /home/travis/build/Neder/Towny
 echo "Pull Request Number(false = Master Build) => ${TRAVIS_PULL_REQUEST}"
+name=$(date +%Y%m%d)
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     sudo curl -T ./out/Towny_${name}_CP949_${TRAVIS_COMMIT}.jar -u u833935363:TownyTowny ftp://ftp.ocw5902.w.pw/Towny/
     sudo curl -T ./out/Towny_${name}_UTF-8_${TRAVIS_COMMIT}.jar -u u833935363:TownyTowny ftp://ftp.ocw5902.w.pw/Towny/
