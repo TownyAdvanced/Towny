4ee581277caa461cdb60f2d0fbc8948b110dbd3f
Neder <ocw5902@naver.com>
2014-05-11 01:31:45 +0900

    CI save format change

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 echo "Pull Request Number(false = Master Build) => ${TRAVIS_PULL_REQUEST}"
 name=$(date +%Y%m%d)
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    sudo curl -T ./out/Towny_${name}_CP949_${TRAVIS_COMMIT}_${TRAVIS_JOB_NUMBER}.jar -u u833935363:TownyTowny ftp://ftp.ocw5902.w.pw/Towny/
-    sudo curl -T ./out/Towny_${name}_UTF-8_${TRAVIS_COMMIT}_${TRAVIS_JOB_NUMBER}.jar -u u833935363:TownyTowny ftp://ftp.ocw5902.w.pw/Towny/
+    sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar -u u833935363:TownyTowny ftp://ftp.ocw5902.w.pw/Towny/
+    sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_UTF-8_${TRAVIS_COMMIT}.jar -u u833935363:TownyTowny ftp://ftp.ocw5902.w.pw/Towny/
 else
     echo "It's not master build."
 fi
