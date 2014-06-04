50ddfdf0b5e450c9a72244cc7cd1e55bd05c8dd7
Neder <ocw5902@naver.com>
2014-06-04 15:18:45 +0900

    added travis upload messages

--- upload.sh
+++ upload.sh
@@ -1,7 +1,9 @@
 echo "Pull Request Number(false = Master Build) => ${TRAVIS_PULL_REQUEST}"
 name=$(date +%Y%m%d)
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    echo Uploading Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar ...
     sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar -u u833935363:TownyTowny ftp://ftp.ocw5902.w.pw/Towny/
+    echo Uploading Towny_${TRAVIS_JOB_NUMBER}_${name}_UTF-8_${TRAVIS_COMMIT}.jar ...
     sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_UTF-8_${TRAVIS_COMMIT}.jar -u u833935363:TownyTowny ftp://ftp.ocw5902.w.pw/Towny/
 else
     echo "It's not master build."
