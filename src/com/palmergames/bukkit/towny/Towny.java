7f153dd6eac63c352474406e0e12052ff3309040
Neder <ocw5902@naver.com>
2014-06-12 20:00:06 +0900

    메인명령어 수정

--- src/com/palmergames/bukkit/towny/Towny.java
+++ src/com/palmergames/bukkit/towny/Towny.java
@@ -102,13 +102,13 @@ public class Towny extends JavaPlugin {
 
 		if (load()) {
 			// Setup bukkit command interfaces
-			getCommand("townyadmin").setExecutor(new TownyAdminCommand(this));
-			getCommand("townyworld").setExecutor(new TownyWorldCommand(this));
-			getCommand("resident").setExecutor(new ResidentCommand(this));
-			getCommand("towny").setExecutor(new TownyCommand(this));
-			getCommand("town").setExecutor(new TownCommand(this));
+			getCommand("타우니관리").setExecutor(new TownyAdminCommand(this));
+			getCommand("타우니월드").setExecutor(new TownyWorldCommand(this));
+			getCommand("주민").setExecutor(new ResidentCommand(this));
+			getCommand("타우니").setExecutor(new TownyCommand(this));
+			getCommand("마을").setExecutor(new TownCommand(this));
 			getCommand("국가").setExecutor(new NationCommand(this));
-			getCommand("plot").setExecutor(new PlotCommand(this));
+			getCommand("토지").setExecutor(new PlotCommand(this));
 
 			TownyWar.onEnable();
 
