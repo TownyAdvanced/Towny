deba23c0bee8efd1a8cc63eef4bd0f12031d5605
Neder <ocw5902@naver.com>
2014-08-30 12:56:59 +0900

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,9 +2,9 @@ echo "Pull Request Number(false = Master Build) => ${TRAVIS_PULL_REQUEST}"
 name=$(date +%Y%m%d)
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo Uploading Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar ...
-    sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar -u u833935363:TownyTowny ftp://ftp.ocw5902.w.pw/Towny/
+    sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar -u u262377766.towny:dtdtdtdtdtd ftp://ftp.ocw5902.esy.es/Towny/
     echo Uploading Towny_${TRAVIS_JOB_NUMBER}_${name}_UTF-8_${TRAVIS_COMMIT}.jar ...
-    sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_UTF-8_${TRAVIS_COMMIT}.jar -u u833935363:TownyTowny ftp://ftp.ocw5902.w.pw/Towny/
+    sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_UTF-8_${TRAVIS_COMMIT}.jar -u u262377766.towny:dtdtdtdtdtd ftp://ftp.ocw5902.esy.es/Towny/
 else
     echo "It's not master build."
 fi
