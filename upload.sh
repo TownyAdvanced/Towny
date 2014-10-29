4c66145476a7de923fab2c9124f0b19ec36c294c
Neder <ocw5902@naver.com>
2014-10-29 11:20:05 +0900

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,8 @@
 echo "Pull Request Number(false = Master Build) => ${TRAVIS_PULL_REQUEST}"
 name=$(date +%Y%m%d)
+echo MD5:
+md5sum ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_UTF-8_${TRAVIS_COMMIT}.jar
+md5sum ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo Uploading Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar ...
     sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar -u u262377766.towny:dtdtdtdtdtd ftp://ftp.ocw5902.esy.es/Towny/
