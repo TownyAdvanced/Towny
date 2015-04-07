403324b40e0399f60b0981aa719d1244d48162b6
Capsman08 <jam0704@vt.eud>
2015-04-07 16:10:11 -0500

    Edges 3.0
    More fixing

--- src/com/palmergames/bukkit/towny/war/eventwar/WarTimerTask.java
+++ src/com/palmergames/bukkit/towny/war/eventwar/WarTimerTask.java
@@ -98,8 +98,8 @@ public class WarTimerTask extends TownyTimerTask {
 			try {
 				TownBlock edgeTownBlock = worldCoord.getTownyWorld().getTownBlock(new Coord(worldCoord.getX() + offset[i][0], worldCoord.getZ() + offset[i][1]));
 				boolean sameTown = edgeTownBlock.getTown() == townBlock.getTown();
-				TownyMessaging.sendDebugMsg("[WAR] Ole8pieTesting: (For townBlock:" + townBlock.getCoord().toString() + ")  SameTown:" + sameTown + "  IsWarZone:" + warEvent.isWarZone(worldCoord));
-				if (!sameTown || (sameTown && !warEvent.isWarZone(worldCoord))) {
+				TownyMessaging.sendDebugMsg("[WAR] Ole8pieTesting: (For townBlock:" + edgeTownBlock.getCoord().toString() + ")  SameTown:" + sameTown + "  IsWarZone:" + warEvent.isWarZone(edgeTownBlock.getWorldCoord()));
+				if (!sameTown || (sameTown && !warEvent.isWarZone(edgeTownBlock.getWorldCoord()))) {
 					return true;
 				}
 			} catch (NotRegisteredException e) {
