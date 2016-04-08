8e313e75c353ec2d2c55bf350cd792da47c3fe79
Neder <ocw5902@naver.com>
2016-04-08 22:09:47 +0900

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -5,9 +5,9 @@ md5sum ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_UTF-8_${TRAVIS_COMMIT}.jar
 md5sum ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo Uploading Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar ...
-    sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar -u ${username}:${password} ${host}
+    sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_CP949_${TRAVIS_COMMIT}.jar -u ${username}:${password} ftp://${host}
     echo Uploading Towny_${TRAVIS_JOB_NUMBER}_${name}_UTF-8_${TRAVIS_COMMIT}.jar ...
-    sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_UTF-8_${TRAVIS_COMMIT}.jar -u ${username}:${password} ${host}
+    sudo curl -T ./out/Towny_${TRAVIS_JOB_NUMBER}_${name}_UTF-8_${TRAVIS_COMMIT}.jar -u ${username}:${password} ftp://${host}
 else
     echo "It's not master build."
 fi
