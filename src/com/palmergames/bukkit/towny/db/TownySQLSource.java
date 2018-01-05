550d2212f4f1619cc5dccdc2a19f04c180d39571
Wolf2323 <gabrielpatrikuban@gmail.com>
2018-01-05 12:31:55 +0100

    Revert "Update TownySQLSource.java"
    This reverts commit 36dfe55ea6a25a9c84805ee0463eb4e749ce566a.

--- src/com/palmergames/bukkit/towny/db/TownySQLSource.java
+++ src/com/palmergames/bukkit/towny/db/TownySQLSource.java
@@ -1756,7 +1756,6 @@ public class TownySQLSource extends TownyFlatFileSource {
             e.printStackTrace();
         }
         return true;
-    }
 
     @Override
     public void deleteResident(Resident resident) {
