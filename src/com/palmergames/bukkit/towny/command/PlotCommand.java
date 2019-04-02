e878ccde3d13b86137c74bd5abe91bb28d0d66c7
TownyAdvanced <LlmDl@palmergames.com>
2019-04-02 13:15:15 -0500

    Fix typo

--- src/com/palmergames/bukkit/towny/command/PlotCommand.java
+++ src/com/palmergames/bukkit/towny/command/PlotCommand.java
@@ -317,7 +317,7 @@ public class PlotCommand extends BaseCommand implements CommandExecutor {
 						if (!TownyUniverse.getPermissionSource().testPermission(player, PermissionNodes.TOWNY_COMMAND_PLOT_PERM_HUD.getNode()))
 							throw new TownyException(TownySettings.getLangString("msg_err_command_disable"));
 						
-						plugin.getHUDManager().togglePermHuD(player);
+						plugin.getHUDManager().togglePermHUD(player);
 						
 					} else {
 						TownBlock townBlock = new WorldCoord(world, Coord.parseCoord(player)).getTownBlock();
