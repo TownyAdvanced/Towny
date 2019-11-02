c8d3434934e7e674bb01fac5e68bb92452acfbe4
suneettipirneni <W48-Qh3-mFF-X3V>
2019-11-02 14:56:04 -0400

    Converted new classes to use spaces instead of tabs.

--- src/com/palmergames/bukkit/towny/exceptions/MetaDataSerializationException.java
+++ src/com/palmergames/bukkit/towny/exceptions/MetaDataSerializationException.java
@@ -3,5 +3,5 @@ package com.palmergames.bukkit.towny.exceptions;
 import com.palmergames.bukkit.towny.exceptions.TownyException;
 
 public class MetaDataSerializationException extends TownyException {
-	
+    
 }
