a157dec3acf0630c081ec6620f5989ddc35ebfed
suneettipirneni <W48-Qh3-mFF-X3V>
2019-11-24 14:13:02 -0500

    Added newAmount getter to the transaction events

--- src/com/palmergames/bukkit/towny/event/NationPreTransactionEvent.java
+++ src/com/palmergames/bukkit/towny/event/NationPreTransactionEvent.java
@@ -1,7 +1,9 @@
 package com.palmergames.bukkit.towny.event;
 
+import com.palmergames.bukkit.towny.exceptions.EconomyException;
 import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.object.Transaction;
+import com.palmergames.bukkit.util.BukkitTools;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
@@ -54,5 +56,22 @@ public class NationPreTransactionEvent extends Event implements Cancellable {
 
 	public void setCancelMessage(String cancelMessage) {
 		this.cancelMessage = cancelMessage;
+	}
+
+	public int getNewAmount() {
+		try {
+			switch (transaction.getType()) {
+				case DEPOSIT:
+					return (int) (nation.getHoldingBalance() + transaction.getAmount());
+				case WITHDRAW:
+					return (int) (nation.getHoldingBalance() - transaction.getAmount());
+				default:
+					break;
+			}
+		} catch (EconomyException e) {
+			BukkitTools.getServer().getLogger().warning(e.getMessage());
+		}
+		
+		return 0;
 	}
 }
--- src/com/palmergames/bukkit/towny/event/PlayerPreTransactionEvent.java
+++ src/com/palmergames/bukkit/towny/event/PlayerPreTransactionEvent.java
@@ -1,5 +1,6 @@
 package com.palmergames.bukkit.towny.event;
 
+import com.palmergames.bukkit.towny.TownyEconomyHandler;
 import com.palmergames.bukkit.towny.object.Transaction;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Cancellable;
@@ -43,5 +44,22 @@ public class PlayerPreTransactionEvent extends Event implements Cancellable {
 
 	public Transaction getTransaction() {
 		return transaction;
+	}
+	
+	public int getNewAmount() {
+		switch (transaction.getType()) {
+			case ADD:
+				return (int) (TownyEconomyHandler.getBalance(transaction.getPlayer().getName(),
+					transaction.getPlayer().getWorld())
+					+ transaction.getAmount());
+			case SUBTRACT:
+				return (int) (TownyEconomyHandler.getBalance(transaction.getPlayer().getName(),
+					transaction.getPlayer().getWorld())
+					- transaction.getAmount());
+			default:
+				break;
+		}
+		
+		return 0;
 	}
 }
