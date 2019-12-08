c877f2fb022e82f16acd300864bc6468b8b75a05
suneettipirneni <suneettipirneni@icloud.com>
2019-12-08 18:08:01 -0500

    Added initial code for group remove confirmation.

--- src/com/palmergames/bukkit/towny/confirmations/ConfirmationHandler.java
+++ src/com/palmergames/bukkit/towny/confirmations/ConfirmationHandler.java
@@ -37,124 +37,151 @@ public class ConfirmationHandler {
 	private static HashMap<Resident, String> townypurgeconfirmations = new HashMap<>();
 	private static HashMap<Resident, Nation> nationmergeconfirmations = new HashMap<>();
 	private static HashMap<Resident, GroupConfirmation> groupclaimconfirmations = new HashMap<>();
+	private static HashMap<Resident, GroupConfirmation> groupremoveconfirmations = new HashMap<>();
 	public static ConfirmationType consoleConfirmationType = ConfirmationType.NULL;
 	private static Object consoleExtra = null;
 
 	public static void addConfirmation(final Resident r, final ConfirmationType type, Object extra) throws TownyException {
 		// We use "extra" in certain instances like the number of days for something e.t.c
-		if (type == ConfirmationType.TOWNDELETE) {
-			r.setConfirmationType(type);
-			towndeleteconfirmations.put(r, r.getTown()); // The good thing is, using the "put" option we override the past one!
+		switch (type) {
+			case TOWNDELETE:
+				r.setConfirmationType(type);
+				towndeleteconfirmations.put(r, r.getTown()); // The good thing is, using the "put" option we override the past one!
 
-			new BukkitRunnable() {
-				@Override
-				public void run() {
-					removeConfirmation(r, type, false);
-				}
-			}.runTaskLater(plugin, 400);
-		}
-		if (type == ConfirmationType.PURGE) {
-			r.setConfirmationType(type);
-			townypurgeconfirmations.put(r, (String) extra); // However an add option doesn't overridee so, we need to check if it exists first.
-			new BukkitRunnable() {
-				@Override
-				public void run() {
-					removeConfirmation(r, type, false);
-				}
-			}.runTaskLater(plugin, 400);
-		}
-		if (type == ConfirmationType.UNCLAIMALL) {
 
-			r.setConfirmationType(type);
-			townunclaimallconfirmations.put(r, r.getTown());
+				new BukkitRunnable() {
+					@Override
+					public void run() {
+						removeConfirmation(r, type, false);
+					}
+				}.runTaskLater(plugin, 400);
+				break;
+			case NATIONDELETE:
+				r.setConfirmationType(type);
+				nationdeleteconfirmations.put(r, r.getTown().getNation());
 
-			new BukkitRunnable() {
-				@Override
-				public void run() {
-					removeConfirmation(r, type, false);
-				}
-			}.runTaskLater(plugin, 400);
-		}
-		if (type == ConfirmationType.NATIONDELETE) {
+				new BukkitRunnable() {
+					@Override
+					public void run() {
+						removeConfirmation(r, type, false);
+					}
+				}.runTaskLater(plugin, 400);
+				break;
+			case UNCLAIMALL:
+				r.setConfirmationType(type);
+				townunclaimallconfirmations.put(r, r.getTown());
 
-			r.setConfirmationType(type);
-			nationdeleteconfirmations.put(r, r.getTown().getNation());
+				new BukkitRunnable() {
+					@Override
+					public void run() {
+						removeConfirmation(r, type, false);
+					}
+				}.runTaskLater(plugin, 400);
+				break;
+			case PURGE:
+				r.setConfirmationType(type);
+				townypurgeconfirmations.put(r, (String) extra); // However an add option doesn't overridee so, we need to check if it exists first.
+				new BukkitRunnable() {
+					@Override
+					public void run() {
+						removeConfirmation(r, type, false);
+					}
+				}.runTaskLater(plugin, 400);
+				break;
+			case NATIONMERGE:
+				r.setConfirmationType(type);
+				nationmergeconfirmations.put(r, (Nation) extra);
 
-			new BukkitRunnable() {
-				@Override
-				public void run() {
-					removeConfirmation(r, type, false);
-				}
-			}.runTaskLater(plugin, 400);
-		}
-		if (type == ConfirmationType.NATIONMERGE) {
-			r.setConfirmationType(type);
-			nationmergeconfirmations.put(r, (Nation) extra);
-			
-			new BukkitRunnable() {
-				@Override
-				public void run() {
-					removeConfirmation(r, type, false);
-				}
-			}.runTaskLater(plugin, 400);
-		}
-		if (type == ConfirmationType.GROUPCLAIMACTION) {
-			r.setConfirmationType(type);
-			groupclaimconfirmations.put(r, (GroupConfirmation) extra);
+				new BukkitRunnable() {
+					@Override
+					public void run() {
+						removeConfirmation(r, type, false);
+					}
+				}.runTaskLater(plugin, 400);
+				break;
+			case NULL:
+				break;
+			case GROUPCLAIMACTION:
+				r.setConfirmationType(type);
+				groupclaimconfirmations.put(r, (GroupConfirmation) extra);
 
-			new BukkitRunnable() {
-				@Override
-				public void run() {
-					removeConfirmation(r, type, false);
-				}
-			}.runTaskLater(plugin, 400);
+				new BukkitRunnable() {
+					@Override
+					public void run() {
+						removeConfirmation(r, type, false);
+					}
+				}.runTaskLater(plugin, 400);
+				break;
+			case GROUPUNCLAIMACTION:
+				r.setConfirmationType(type);
+				groupremoveconfirmations.put(r, (GroupConfirmation) extra);
+
+				new BukkitRunnable() {
+					@Override
+					public void run() {
+						removeConfirmation(r, type, false);
+					}
+				}.runTaskLater(plugin, 400);
+				break;
 		}
 	}
 
 	public static void removeConfirmation(Resident r, ConfirmationType type, boolean successful) {
 		boolean sendmessage = false;
-		if (type == ConfirmationType.TOWNDELETE) {
-			if (towndeleteconfirmations.containsKey(r) && !successful) {
-				sendmessage = true;
-			}
-			towndeleteconfirmations.remove(r);
-			r.setConfirmationType(null);
+		switch (type) {
+			case TOWNDELETE:
+				if (towndeleteconfirmations.containsKey(r) && !successful) {
+					sendmessage = true;
+				}
+				towndeleteconfirmations.remove(r);
+				r.setConfirmationType(null);
+				break;
+			case NATIONDELETE:
+				if (nationdeleteconfirmations.containsKey(r) && !successful) {
+					sendmessage = true;
+				}
+				nationdeleteconfirmations.remove(r);
+				r.setConfirmationType(null);
+				break;
+			case UNCLAIMALL:
+				if (townunclaimallconfirmations.containsKey(r) && !successful) {
+					sendmessage = true;
+				}
+				townunclaimallconfirmations.remove(r);
+				r.setConfirmationType(null);
+				break;
+			case PURGE:
+				if (townypurgeconfirmations.containsKey(r) && !successful) {
+					sendmessage = true;
+				}
+				townypurgeconfirmations.remove(r);
+				r.setConfirmationType(null);
+				break;
+			case NATIONMERGE:
+				if (nationmergeconfirmations.containsKey(r) && !successful) {
+					sendmessage = true;
+				}
+				nationmergeconfirmations.remove(r);
+				r.setConfirmationType(null);
+				break;
+			case NULL:
+				break;
+			case GROUPCLAIMACTION:
+				if (groupclaimconfirmations.containsKey(r) && !successful) {
+					sendmessage = true;
+				}
+				groupclaimconfirmations.remove(r);
+				r.setConfirmationType(null);
+				break;
+			case GROUPUNCLAIMACTION:
+				if (groupremoveconfirmations.containsKey(r) && !successful) {
+					sendmessage = true;
+				}
+				groupclaimconfirmations.remove(r);
+				r.setConfirmationType(null);
+				break;
 		}
-		if (type == ConfirmationType.PURGE) {
-			if (townypurgeconfirmations.containsKey(r) && !successful) {
-				sendmessage = true;
-			}
-			townypurgeconfirmations.remove(r);
-			r.setConfirmationType(null);
-		}
-		if (type == ConfirmationType.UNCLAIMALL) {
-			if (townunclaimallconfirmations.containsKey(r) && !successful) {
-				sendmessage = true;
-			}
-			townunclaimallconfirmations.remove(r);
-			r.setConfirmationType(null);
-		}
-		if (type == ConfirmationType.NATIONDELETE) {
-			if (nationdeleteconfirmations.containsKey(r) && !successful) {
-				sendmessage = true;
-			}
-			nationdeleteconfirmations.remove(r);
-			r.setConfirmationType(null);
-		}
-		if (type == ConfirmationType.NATIONMERGE) {
-			if (nationmergeconfirmations.containsKey(r) && !successful) {
-				sendmessage = true;
-			}
-			nationmergeconfirmations.remove(r);
-			r.setConfirmationType(null);
-		}
-		if (type == ConfirmationType.GROUPCLAIMACTION) {
-			if (groupclaimconfirmations.containsKey(r) && !successful) {
-				sendmessage = true;
-			}
-			groupclaimconfirmations.remove(r);
-			r.setConfirmationType(null);
-		}
+		
 		if (sendmessage) {
 			TownyMessaging.sendMsg(r, TownySettings.getLangString("successful_cancel"));
 		}
@@ -232,6 +259,20 @@ public class ConfirmationHandler {
 					coords.add(tb.getWorldCoord());
 				}
 				new PlotClaim(Towny.getPlugin(), confirmation.getPlayer(), r, coords, true, false).start();
+				removeConfirmation(r, type, true);
+			}
+		}
+		
+		if (type == ConfirmationType.GROUPUNCLAIMACTION) {
+			if (groupremoveconfirmations.containsKey(r)) {
+				ArrayList<WorldCoord> coords = new ArrayList<>();
+				GroupConfirmation confirmation = groupremoveconfirmations.get(r);
+				ArrayList<TownBlock> townBlocks = (ArrayList<TownBlock>) confirmation.getGroup().getTownBlocks();
+				for (TownBlock tb :townBlocks) {
+					TownyMessaging.sendErrorMsg(tb.getCoord().toString());
+					coords.add(tb.getWorldCoord());
+				}
+				new PlotClaim(Towny.getPlugin(), confirmation.getPlayer(), r, coords, false, false).start();
 				removeConfirmation(r, type, true);
 			}
 		}
