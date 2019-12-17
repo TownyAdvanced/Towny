64147a642898cc38bda142d6a7d8ac462370b67b
Llm Dl <LlmDlio@gmail.com>
2019-12-17 15:35:16 -0600

    Fix up doublechests and explregen.

--- src/com/palmergames/bukkit/towny/tasks/ProtectionRegenTask.java
+++ src/com/palmergames/bukkit/towny/tasks/ProtectionRegenTask.java
@@ -6,9 +6,13 @@ import com.palmergames.bukkit.towny.regen.block.BlockLocation;
 
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
+import org.bukkit.block.DoubleChest;
+import org.bukkit.block.Chest;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.inventory.BlockInventoryHolder;
+import org.bukkit.inventory.DoubleChestInventory;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
 public class ProtectionRegenTask extends TownyTimerTask {
@@ -25,15 +29,22 @@ public class ProtectionRegenTask extends TownyTimerTask {
 		this.setBlockLocation(new BlockLocation(block.getLocation()));
 
 		if (state instanceof BlockInventoryHolder) {
-			Inventory inven = ((BlockInventoryHolder) state).getInventory(); 
-			items = inven.getContents();				
-			inven.clear();				
+			Inventory inven = ((BlockInventoryHolder) state).getInventory();
+			
+			// Because DoubleChests are special we have to handle their inventory in two parts.
+			if (inven instanceof DoubleChestInventory) {
+				Inventory dblChestInven = ((Chest) state).getBlockInventory(); // .getBlockInventory() grabs only the 1/2 of the DoubleChest we want.
+				items = dblChestInven.getContents();
+				dblChestInven.clear();
+			} else { // Handle all other BlockInventoryHolders.				
+				items = inven.getContents();
+				inven.clear();
+			}			
 		}
 	}
 
 	@Override
 	public void run() {
-
 		replaceProtections();
 		TownyRegenAPI.removeProtectionRegenTask(this);
 	}
@@ -45,8 +56,14 @@ public class ProtectionRegenTask extends TownyTimerTask {
 			BlockData blockData = state.getBlockData().clone();			
 			block.setType(state.getType(), false);
 			block.setBlockData(blockData);
-			if (block.getState() instanceof BlockInventoryHolder)
-				((BlockInventoryHolder) block.getState()).getInventory().setContents(items);
+			if (block.getState() instanceof BlockInventoryHolder) {
+				InventoryHolder inven = ((BlockInventoryHolder) state).getInventory().getHolder();
+				if (inven instanceof DoubleChest) {
+					((Chest) block.getState()).getBlockInventory().setContents(items);
+				} else {
+					((BlockInventoryHolder) block.getState()).getInventory().setContents(items);					
+				}
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
