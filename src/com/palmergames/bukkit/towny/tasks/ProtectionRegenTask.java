fa09c71d2f9f376ad8c5ed0e6d6687383396e5de
FlagCourier <FlagCourier@pm.me>
2020-01-08 12:28:11 -0600

    Fix regen pig problem (#3629)
    * fix preclaim event not firing on town creation.
    
    * Possible fix for spawners regenerating as pig spawners.
    
    * Possible fix for spawners regenerating as pig spawners.
    
    * Revert "fix preclaim event not firing on town creation."
    
    This reverts commit 65511a24

--- src/com/palmergames/bukkit/towny/tasks/ProtectionRegenTask.java
+++ src/com/palmergames/bukkit/towny/tasks/ProtectionRegenTask.java
@@ -7,7 +7,9 @@ import com.palmergames.bukkit.towny.regen.block.BlockLocation;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
+import org.bukkit.block.CreatureSpawner;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
@@ -66,6 +68,35 @@ public class ProtectionRegenTask extends TownyTimerTask {
 			block.setBlockData(blockData);
 		} catch (Exception ex) {
 			ex.printStackTrace();
+		}
+		
+		// If the state is a creature spawner, then replace properly.
+		if (state instanceof CreatureSpawner) {
+			// Up cast to interface.
+			CreatureSpawner spawner = (CreatureSpawner) state;
+			
+			// Capture spawn type and set it.
+			EntityType type = spawner.getSpawnedType();
+			((CreatureSpawner) state).setSpawnedType(type);
+
+			// update blocks.
+			state.update();
+		}
+		
+		// Add inventory back to the block if it conforms to BlockInventoryHolder.
+		if (state instanceof BlockInventoryHolder) {
+			// Up cast to interface.
+			BlockInventoryHolder container = (BlockInventoryHolder) state;
+			
+			// Check for chest.
+			if (container instanceof Chest) {
+				((Chest) state).getBlockInventory().setContents(contents);
+			} else {
+				((BlockInventoryHolder) state).getInventory().setContents(contents);
+			}
+			
+			// update blocks.
+			state.update();
 		}
 	}
 
