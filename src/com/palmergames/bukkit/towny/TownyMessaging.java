128a66d9db45b7e318fa277a0ffaef08fd9a01f6
Suneet Tipirneni <suneettipirneni@icloud.com>
2020-01-13 11:14:41 -0500

    Fixed hypen still being green

--- src/com/palmergames/bukkit/towny/TownyMessaging.java
+++ src/com/palmergames/bukkit/towny/TownyMessaging.java
@@ -719,7 +719,7 @@ public class TownyMessaging {
 		// Use spigot to send the message.
 		player.spigot().sendMessage(new ComponentBuilder(firstline + "\n")
 			.append(confirmComponent).append(ChatColor.WHITE + " - " + String.format(TownySettings.getLangString("msg_confirmation_spigot_click_accept"), confirmline.replace('/', '[').replace("[",""), confirmline) + "\n")
-			.append(cancelComponent).append(" - " + ChatColor.WHITE + String.format(TownySettings.getLangString("msg_confirmation_spigot_click_cancel"), cancelline.replace('/', '['), cancelline).replace("[","") + "\n")
+			.append(cancelComponent).append(ChatColor.WHITE + " - " + String.format(TownySettings.getLangString("msg_confirmation_spigot_click_cancel"), cancelline.replace('/', '['), cancelline).replace("[","") + "\n")
 			.append(lastline)
 			.create());
 	}
