675295aa883f6f7ce079e06980997899075fe424
Suneet Tipirneni <suneettipirneni@icloud.com>
2020-01-15 14:39:23 -0500

    Fixed outdated stuffs

--- src/com/palmergames/bukkit/towny/TownySpigotMessaging.java
+++ src/com/palmergames/bukkit/towny/TownySpigotMessaging.java
@@ -63,8 +63,7 @@ public class TownySpigotMessaging {
 		TextComponent cancelComponent = new TextComponent(ChatColor.GREEN + cancelline.replace('/', '[').concat("]"));
 		cancelComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(TownySettings.getLangString("msg_confirmation_spigot_hover_cancel")).create()));
 		cancelComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, cancelline));
-
-		TownyMessaging.sendErrorMsg(cancelline);
+		
 		// Use spigot to send the message.
 		player.spigot().sendMessage(new ComponentBuilder(firstline + "\n")
 			.append(confirmComponent).append(ChatColor.WHITE + " - " + String.format(TownySettings.getLangString("msg_confirmation_spigot_click_accept"), confirmline.replace('/', '[').replace("[",""), confirmline) + "\n")
