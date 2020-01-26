f6d1c67985a4fced3322f7747c3f776f9b5c9046
Goosius1 <goosius1@gmail.com>
2020-01-26 23:00:08 +0000

    siegewar - configured more blocks marked unstable
    - Logs can be unstable too, thus possible banner breakage
    - Added a few more existing enum entries to the unstable list
    - Confirmed compilation ok

--- src/com/palmergames/bukkit/towny/war/siegewar/utils/SiegeWarBlockUtil.java
+++ src/com/palmergames/bukkit/towny/war/siegewar/utils/SiegeWarBlockUtil.java
@@ -200,6 +200,18 @@ public class SiegeWarBlockUtil {
 			case SAND:
 			case SOUL_SAND:
 			case RED_SAND:
+			case ACACIA_LOG:
+			case BIRCH_LOG:
+			case DARK_OAK_LOG:
+			case JUNGLE_LOG:
+			case OAK_LOG:
+			case SPRUCE_LOG:
+			case STRIPPED_ACACIA_LOG:
+			case STRIPPED_BIRCH_LOG:
+			case STRIPPED_DARK_OAK_LOG:
+			case STRIPPED_JUNGLE_LOG:
+			case STRIPPED_OAK_LOG:
+			case STRIPPED_SPRUCE_LOG:
 				return true;
 			default:
 				return false;
