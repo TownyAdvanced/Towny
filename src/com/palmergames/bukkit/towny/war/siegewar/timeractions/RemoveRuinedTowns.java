1736ab2731e4d7634fe2a98cfaaf404339bc60dd
goosius <>
2020-02-04 12:17:07 +0000

    siegewar - fix for ruins-not-deleting
    - Brought the ruins deleting pattern much more into line with the look
    of the other timer functions
    - It is hoped that this will fix the ruins-not-deleting bug
    - Even though it works locally, there may be some combination of issues
    on a larger server.
    - If this does not work then it probably makes sense to start looking at
    logs etc.

--- src/com/palmergames/bukkit/towny/war/siegewar/timeractions/RemoveRuinedTowns.java
+++ src/com/palmergames/bukkit/towny/war/siegewar/timeractions/RemoveRuinedTowns.java
@@ -4,6 +4,8 @@ import com.palmergames.bukkit.towny.TownyUniverse;
 import com.palmergames.bukkit.towny.object.Town;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  * This class is responsible for removing ruined towns completely
@@ -17,17 +19,30 @@ public class RemoveRuinedTowns {
 	 * It determines which towns have lain in ruins for long enough, and deletes them.
 	 */
     public static void removeRuinedTowns() {
-		TownyUniverse universe = TownyUniverse.getInstance();
-        for (Town town : new ArrayList<>(universe.getDataSource().getTowns())) {
-            if(town.isRuined()) {
-            	if(town.getRecentlyRuinedEndTime() != 999) {
-            		//Prepare to delete in next cycle. 999 is just an arbitrary number to signify delete
-					town.setRecentlyRuinedEndTime(999);
-					universe.getDataSource().saveTown(town);
-				} else {
-					universe.getDataSource().removeRuinedTown(town);
+		TownyUniverse townyUniverse = TownyUniverse.getInstance();
+		List<Town> towns = new ArrayList<>(townyUniverse.getDataSource().getTowns());
+		ListIterator<Town> townItr = towns.listIterator();
+		Town town;
+
+		while (townItr.hasNext()) {
+			town = townItr.next();
+			/*
+			 * Only remove ruined town if it really still
+			 * exists.
+			 * We are running in an Async thread so MUST verify all objects.
+			 */
+			if (townyUniverse.getDataSource().hasTown(town.getName())) {
+				if(town.isRuined()) {
+					if(town.getRecentlyRuinedEndTime() != 999) {
+						//Prepare to delete in next cycle. 999 is just an arbitrary number to signify delete
+						town.setRecentlyRuinedEndTime(999);
+						townyUniverse.getDataSource().saveTown(town);
+					} else {
+						townyUniverse.getDataSource().removeRuinedTown(town);
+					}
 				}
-            }
-        }
+			} 
+		}
     }
+    
 }
