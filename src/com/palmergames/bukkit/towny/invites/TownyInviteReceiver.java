858d115d45395ead0317bf1bf16b0a35982cdd25
Suneet Tipirneni (Siris) <suneettipirneni@icloud.com>
2020-02-19 22:16:00 -0500

    Add .ds_store

--- src/com/palmergames/bukkit/towny/invites/TownyInviteReceiver.java
+++ src/com/palmergames/bukkit/towny/invites/TownyInviteReceiver.java
@@ -1,15 +1,14 @@
 package com.palmergames.bukkit.towny.invites;
 
 import com.palmergames.bukkit.towny.invites.exceptions.TooManyInvitesException;
+import com.palmergames.bukkit.towny.object.Nameable;
 
 import java.util.List;
 
 /**
  * @author Articdive
  */
-public interface TownyInviteReceiver {
-
-	String getName();
+public interface TownyInviteReceiver extends Nameable {
 
 	List<Invite> getReceivedInvites();
 
