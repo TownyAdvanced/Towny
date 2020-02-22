605ef9ded3d914559e4b922fc37ca6abf96edea1
BuildTools <lemonpickles1558@gmail.com>
2020-02-21 21:08:38 -0500

    Unused import

--- src/com/palmergames/bukkit/towny/TownyUniverse.java
+++ src/com/palmergames/bukkit/towny/TownyUniverse.java
@@ -36,7 +36,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Towny's class for internal API Methods
