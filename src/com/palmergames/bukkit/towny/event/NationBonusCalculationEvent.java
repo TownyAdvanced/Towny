0ebe9c7545cfbca1a26d2fc0e4e146cc1af35777
silverwolfg11 <silverwolfg11@gmail.com>
2020-04-04 16:26:37 -0700

    Nation Bonus Event (#3873)
    * Nation Bonus Event
    
    * Actually call the event

---
+++ src/com/palmergames/bukkit/towny/event/NationBonusCalculationEvent.java
@@ -0,0 +1,41 @@
+package com.palmergames.bukkit.towny.event;
+
+import com.palmergames.bukkit.towny.object.Nation;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Event called whenever nation bonus blocks are being fetched.
+ */
+public class NationBonusCalculationEvent extends Event {
+	private static HandlerList handlers = new HandlerList();
+	
+	private Nation nation;
+	private int bonusBlocks;
+	
+	public NationBonusCalculationEvent(Nation nation, int bonusBlocks) {
+		this.nation = nation;
+		this.bonusBlocks = bonusBlocks;
+	}
+	
+	public Nation getNation() {
+		return nation;
+	}
+		
+	public int getBonusBlocks() {
+		return bonusBlocks;
+	}
+	
+	public void setBonusBlocks(int bonusBlocks) {
+		this.bonusBlocks = bonusBlocks;
+	}
+	
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+}
