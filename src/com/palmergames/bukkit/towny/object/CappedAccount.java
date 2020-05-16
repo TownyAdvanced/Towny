66547986e6d3a12f1f41ddeba58a9dc56de6db2e
Suneet Tipirneni (Siris) <suneettipirneni@icloud.com>
2020-05-16 12:25:13 -0400

    Add observer for account transactions to separate auditing code.

--- src/com/palmergames/bukkit/towny/object/CappedAccount.java
+++ src/com/palmergames/bukkit/towny/object/CappedAccount.java
@@ -1,5 +1,6 @@
 package com.palmergames.bukkit.towny.object;
 
+import com.palmergames.bukkit.towny.TownyEconomyHandler;
 import com.palmergames.bukkit.towny.exceptions.EconomyException;
 import org.bukkit.World;
 
@@ -11,29 +12,29 @@ import java.util.Map;
  * A variant of an account that implements
  * a checked cap on it's balance.
  */
-public class CappedAccount extends Account {
+public class CappedAccount extends Account implements AccountObserver {
 	
 	double cap;
-	Map<Date, Transaction> audits = new HashMap<>();
 	
 	CappedAccount(String name, World world, double cap) {
 		super(name, world);
 		this.cap = cap;
 	}
 	
-	private boolean canAdd(double amount) throws EconomyException {
+	public boolean canAdd(double amount) throws EconomyException {
 		if (cap == 0) {
 			return true;
 		}
 		return !(getHoldingBalance() + amount > cap);
-	} 
+	}
 
 	@Override
-	public boolean add(double amount, String reason) throws EconomyException {
-		if (!canAdd(amount)) {
-			// TODO: Lang String
-			throw new EconomyException("Cannot add above Account Cap.");
-		}
-		return super.add(amount, reason);
+	public void withdrew(Account account, double amount, String reason) {
+		
+	}
+
+	@Override
+	public void deposited(Account account, double amount, String reason) {
+
 	}
 }
