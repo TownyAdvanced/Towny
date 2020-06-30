f614a77db2436628b31babd2d624c9dc9f065273
Silverwolfg11 <silverwolfg11@gmail.com>
2020-06-30 11:54:18 -0700

    Fix missing explicit column defs on resident and nation handlers.

--- src/com/palmergames/bukkit/towny/database/dbHandlers/NationHandler.java
+++ src/com/palmergames/bukkit/towny/database/dbHandlers/NationHandler.java
@@ -1,12 +1,16 @@
 package com.palmergames.bukkit.towny.database.dbHandlers;
 
 import com.palmergames.bukkit.towny.TownyUniverse;
+import com.palmergames.bukkit.towny.database.handler.SQLStringType;
+import com.palmergames.bukkit.towny.database.handler.SaveContext;
+import com.palmergames.bukkit.towny.database.handler.SerializationHandler;
+import com.palmergames.bukkit.towny.database.handler.annotations.SQLString;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.database.handler.LoadContext;
 import com.palmergames.bukkit.towny.database.handler.LoadHandler;
 
-public class NationHandler implements LoadHandler<Nation> {
+public class NationHandler implements SerializationHandler<Nation> {
 	@Override
 	public Nation loadString(LoadContext context, String str) {
 		try {
@@ -14,5 +18,11 @@ public class NationHandler implements LoadHandler<Nation> {
 		} catch (NotRegisteredException ignore) {
 		}
 		return null;
+	}
+
+	@Override
+	@SQLString(stringType = SQLStringType.VARCHAR, length = 36)
+	public String toStoredString(SaveContext context, Nation obj) {
+		return obj.getUniqueIdentifier().toString();
 	}
 }
--- src/com/palmergames/bukkit/towny/database/dbHandlers/ResidentHandler.java
+++ src/com/palmergames/bukkit/towny/database/dbHandlers/ResidentHandler.java
@@ -2,8 +2,10 @@ package com.palmergames.bukkit.towny.database.dbHandlers;
 
 import com.palmergames.bukkit.towny.TownyUniverse;
 import com.palmergames.bukkit.towny.database.handler.LoadContext;
+import com.palmergames.bukkit.towny.database.handler.SQLStringType;
 import com.palmergames.bukkit.towny.database.handler.SaveContext;
 import com.palmergames.bukkit.towny.database.handler.SerializationHandler;
+import com.palmergames.bukkit.towny.database.handler.annotations.SQLString;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.object.Resident;
 
@@ -12,6 +14,7 @@ import java.util.UUID;
 public class ResidentHandler implements SerializationHandler<Resident> {
 
 	@Override
+	@SQLString(stringType = SQLStringType.VARCHAR, length = 36)
 	public String toStoredString(SaveContext context, Resident obj) {
 		return obj.getUniqueIdentifier().toString();
 	}
