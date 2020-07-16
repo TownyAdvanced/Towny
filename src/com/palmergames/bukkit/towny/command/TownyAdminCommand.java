a9ffa96187a79f5427751897701d1c5faaa62bb8
Suneet Tipirneni (Siris) <suneettipirneni@icloud.com>
2020-07-16 14:50:46 -0400

    Move language config code to Translation.java

--- src/com/palmergames/bukkit/towny/command/TownyAdminCommand.java
+++ src/com/palmergames/bukkit/towny/command/TownyAdminCommand.java
@@ -1554,7 +1554,7 @@ public class TownyAdminCommand extends BaseCommand implements CommandExecutor {
 	public void reloadLangs() {
 		String rootFolder = TownyUniverse.getInstance().getRootFolder();
 		try {
-			TownySettings.loadLanguage(rootFolder + File.separator + "settings", "english.yml");
+			Translation.loadLanguage(rootFolder + File.separator + "settings", "english.yml");
 		} catch (IOException e) {
 			TownyMessaging.sendErrorMsg(sender, Translation.of("msg_reload_error"));
 			e.printStackTrace();
@@ -1600,7 +1600,7 @@ public class TownyAdminCommand extends BaseCommand implements CommandExecutor {
 		try {
 			String rootFolder = TownyUniverse.getInstance().getRootFolder();
 			TownySettings.loadConfig(rootFolder + File.separator + "settings" + File.separator + "config.yml", plugin.getVersion());
-			TownySettings.loadLanguage(rootFolder + File.separator + "settings", "english.yml");
+			Translation.loadLanguage(rootFolder + File.separator + "settings", "english.yml");
 		} catch (IOException e) {
 			TownyMessaging.sendErrorMsg(sender, Translation.of("msg_reload_error"));
 			e.printStackTrace();
--- src/com/palmergames/bukkit/towny/object/Translation.java
+++ src/com/palmergames/bukkit/towny/object/Translation.java
@@ -1,11 +1,60 @@
 package com.palmergames.bukkit.towny.object;
 
+import com.palmergames.bukkit.config.CommentedConfiguration;
+import com.palmergames.bukkit.config.ConfigNodes;
+import com.palmergames.bukkit.towny.TownyMessaging;
 import com.palmergames.bukkit.towny.TownySettings;
+import com.palmergames.bukkit.towny.utils.NameUtil;
+import com.palmergames.util.FileMgmt;
+import com.palmergames.util.StringMgmt;
+import org.bukkit.configuration.InvalidConfigurationException;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * A convenience object to facilitate translation. 
  */
 public final class Translation {
+	
+	public static CommentedConfiguration language;
+
+	// This will read the language entry in the config.yml to attempt to load
+	// custom languages
+	// if the file is not found it will load the default from resource
+	public static void loadLanguage(String filepath, String defaultRes) throws IOException {
+
+		String res = TownySettings.getString(ConfigNodes.LANGUAGE.getRoot(), defaultRes);
+		String fullPath = filepath + File.separator + res;
+		File file = FileMgmt.unpackResourceFile(fullPath, res, defaultRes);
+
+		// read the (language).yml into memory
+		language = new CommentedConfiguration(file);
+		language.load();
+		CommentedConfiguration newLanguage = new CommentedConfiguration(file);
+		
+		try {
+			newLanguage.loadFromString(FileMgmt.convertStreamToString("/" + res));
+		} catch (IOException e) {
+			TownyMessaging.sendMsg("Custom language file detected, not updating.");
+			return;
+		} catch (InvalidConfigurationException e) {
+			TownyMessaging.sendMsg("Invalid Configuration in language file detected.");
+		}
+		
+		String resVersion = newLanguage.getString("version");
+		String langVersion = Translation.of("version");
+
+		if (!langVersion.equalsIgnoreCase(resVersion)) {
+			language = newLanguage;
+			TownyMessaging.sendMsg("Newer language file available, language file updated.");
+			FileMgmt.stringToFile(FileMgmt.convertStreamToString("/" + res), file);
+		}
+	}
+
+	private static String parseSingleLineString(String str) {
+		return NameUtil.translateColorCodes(str);
+	}
 	
 	/**
 	 * Translates give key into its respective language. 
@@ -14,7 +63,13 @@ public final class Translation {
 	 * @return The localized string.
 	 */
 	public static String of(String key) {
-		return TownySettings.getLangString(key);
+		String data = language.getString(key.toLowerCase());
+
+		if (data == null) {
+			TownySettings.sendError(key.toLowerCase() + " from " + TownySettings.getString(ConfigNodes.LANGUAGE));
+			return "";
+		}
+		return StringMgmt.translateHexColors(parseSingleLineString(data));
 	}
 
 	/**
