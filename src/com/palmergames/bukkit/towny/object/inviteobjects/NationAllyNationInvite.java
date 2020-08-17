8a4eda9dcf5132d6af76e0b198af0bd0af53e198
Suneet Tipirneni (Siris) <suneettipirneni@icloud.com>
2020-08-16 22:29:39 -0400

    Fix wonky spacing.

--- src/com/palmergames/bukkit/towny/object/inviteobjects/NationAllyNationInvite.java
+++ src/com/palmergames/bukkit/towny/object/inviteobjects/NationAllyNationInvite.java
@@ -15,20 +15,20 @@ public class NationAllyNationInvite extends AbstractInvite<Nation, Nation> {
 
 	@Override
 	public void accept() throws TownyException {
-			Nation receiverNation = getReceiver();
-			Nation senderNation = getSender();
+		Nation receiverNation = getReceiver();
+		Nation senderNation = getSender();
 			
-			receiverNation.addAlly(senderNation);
-			senderNation.addAlly(receiverNation);
+		receiverNation.addAlly(senderNation);
+		senderNation.addAlly(receiverNation);
 			
-			TownyMessaging.sendPrefixedNationMessage(receiverNation, Translation.of("msg_added_ally", senderNation.getName()));
-			TownyMessaging.sendPrefixedNationMessage(senderNation, Translation.of("msg_accept_ally", receiverNation.getName()));
+		TownyMessaging.sendPrefixedNationMessage(receiverNation, Translation.of("msg_added_ally", senderNation.getName()));
+		TownyMessaging.sendPrefixedNationMessage(senderNation, Translation.of("msg_accept_ally", receiverNation.getName()));
 			
-			receiverNation.deleteReceivedInvite(this);
-			senderNation.deleteSentAllyInvite(this);
+		receiverNation.deleteReceivedInvite(this);
+		senderNation.deleteSentAllyInvite(this);
 			
-			TownyUniverse.getInstance().getDataSource().saveNation(receiverNation);
-			TownyUniverse.getInstance().getDataSource().saveNation(senderNation);
+		TownyUniverse.getInstance().getDataSource().saveNation(receiverNation);
+		TownyUniverse.getInstance().getDataSource().saveNation(senderNation);
 	}
 
 	@Override
