89ad5b733b5308765cb99d3f99f4248280b73c50
Suneet Tipirneni (Siris) <suneettipirneni@icloud.com>
2020-08-18 23:09:24 -0400

    Fix more documentation.

--- src/com/palmergames/bukkit/towny/invites/Invite.java
+++ src/com/palmergames/bukkit/towny/invites/Invite.java
@@ -17,28 +17,28 @@ public interface Invite {
 	CommandSender getDirectSender();
 
 	/**
-	 * Gets the receiver of the confirmation.
+	 * Gets the receiver of the invitation.
 	 * 
 	 * @return - The {@link InviteReceiver} object receiving the invite.
 	 */
 	InviteReceiver getReceiver();
 
 	/**
-	 * Gets the sender of the confirmation.
+	 * Gets the sender of the invitation.
 	 * 
 	 * @return - The {@link InviteSender} object that sent the invite.
 	 */
 	InviteSender getSender();
 
 	/**
-	 * Runs the accept code for the given confirmation.
+	 * Runs the accept code for the given invitation.
 	 * 
 	 * @throws TownyException - Sends errors back up to be processed by the caller.
 	 */
 	void accept() throws TownyException;
 
 	/**
-	 * Runs the reject code for the given confirmation.
+	 * Runs the reject code for the given invitation.
 	 * 
 	 * @param fromSender - Tells if invite was revoked (true) or declined (false).
 	 */
