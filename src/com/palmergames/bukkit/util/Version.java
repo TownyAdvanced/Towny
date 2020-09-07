7e51a791bb19a1afcc3be2fe8cf8551ed6362faa
Suneet Tipirneni (Siris) <suneettipirneni@icloud.com>
2020-09-07 12:35:08 -0400

    Make optimizations to version comparisons.

--- src/com/palmergames/bukkit/util/Version.java
+++ src/com/palmergames/bukkit/util/Version.java
@@ -6,17 +6,19 @@ import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Represents a numbering system of a string format.
+ */
 public class Version implements Comparable<Version> {
 
 	private final String version;
 	private static final Pattern SEPARATOR = Pattern.compile("\\.");
 	private static final Pattern VERSION_PATTERN = Pattern.compile("[0-9]+(" + SEPARATOR + "[0-9]+)*");
-	public final String get() {
-		return this.version;
-	}
+	private final String[] components;
 
 	private Version(String version) {
 		this.version = version;
+		components = version.split(SEPARATOR.pattern());
 	}
 
 	/**
@@ -43,13 +45,11 @@ public class Version implements Comparable<Version> {
 
 	@Override 
 	public int compareTo(@NotNull Version that) {
-		String[] thisParts = this.get().split(SEPARATOR.pattern());
-		String[] thatParts = that.get().split(SEPARATOR.pattern());
+		int length = Math.max(components.length, that.components.length);
 		
-		int length = Math.max(thisParts.length, thatParts.length);
 		for(int i = 0; i < length; i++) {
-			int thisPart = i < thisParts.length ? Integer.parseInt(thisParts[i]) : 0;
-			int thatPart = i < thatParts.length ? Integer.parseInt(thatParts[i]) : 0;
+			int thisPart = i < components.length ? Integer.parseInt(components[i]) : 0;
+			int thatPart = i < that.components.length ? Integer.parseInt(that.components[i]) : 0;
 			
 			if (thisPart < thatPart) {
 				return -1;
@@ -78,5 +78,15 @@ public class Version implements Comparable<Version> {
 	@Override
 	public String toString() {
 		return version;
+	}
+
+	/**
+	 * Returns the components of the version separated by .'s
+	 * 
+	 * @return A string array of the components.
+	 */
+	@NotNull
+	public String[] getComponents() {
+		return components;
 	}
 }
