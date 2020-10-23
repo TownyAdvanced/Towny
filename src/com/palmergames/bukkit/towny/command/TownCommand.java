5407ab50a6de5ea6158d27d89e3100ae3ad95d79
Llm Dl <LlmDlio@gmail.com>
2020-10-23 14:19:35 -0500

    - Fix towns being able to get around the maxNumberResidentsWithoutNation setting.

--- src/com/palmergames/bukkit/towny/command/TownCommand.java
+++ src/com/palmergames/bukkit/towny/command/TownCommand.java
@@ -2811,7 +2811,7 @@ public class TownCommand extends BaseCommand implements CommandExecutor, TabComp
 				} else if (!admin && TownySettings.getTownInviteCooldown() > 0 && ( (System.currentTimeMillis()/1000 - newMember.getRegistered()/1000) < (TownySettings.getTownInviteCooldown()) )) {
 					TownyMessaging.sendErrorMsg(sender, Translation.of("msg_err_resident_doesnt_meet_invite_cooldown", newMember));
 					invited.remove(newMember);
-				} else if (TownySettings.getMaxNumResidentsWithoutNation() > 0 && !town.hasNation() && town.getResidents().size() == TownySettings.getMaxNumResidentsWithoutNation()) {
+				} else if (TownySettings.getMaxNumResidentsWithoutNation() > 0 && !town.hasNation() && town.getResidents().size() >= TownySettings.getMaxNumResidentsWithoutNation()) {
 					TownyMessaging.sendErrorMsg(sender, Translation.of("msg_err_unable_to_add_more_residents_without_nation", TownySettings.getMaxNumResidentsWithoutNation()));
 					invited.remove(newMember);
 				} else {
@@ -3048,6 +3048,8 @@ public class TownCommand extends BaseCommand implements CommandExecutor, TabComp
 					throw new Exception(Translation.of("msg_err_not_open", town.getFormattedName()));
 				if (TownySettings.getMaxResidentsPerTown() > 0 && town.getResidents().size() >= TownySettings.getMaxResidentsPerTown())
 					throw new Exception(Translation.of("msg_err_max_residents_per_town_reached", TownySettings.getMaxResidentsPerTown()));
+				if (TownySettings.getMaxNumResidentsWithoutNation() > 0 && !town.hasNation() && town.getResidents().size() >= TownySettings.getMaxNumResidentsWithoutNation())
+					throw new Exception(Translation.of("msg_err_unable_to_add_more_residents_without_nation", TownySettings.getMaxNumResidentsWithoutNation()));
 				if (town.hasOutlaw(resident))
 					throw new Exception(Translation.of("msg_err_outlaw_in_open_town"));
 			}
