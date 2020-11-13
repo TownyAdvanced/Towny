a5824598150b54804512f0d6a66466a63a5eb97a
lucasboss45 <l4f4go@gmail.com>
2020-11-13 16:10:49 +0100

    Moved mob related events to their own package

--- src/com/palmergames/bukkit/towny/event/mobs/MobSpawnRemovalEvent.java
+++ src/com/palmergames/bukkit/towny/event/mobs/MobSpawnRemovalEvent.java
@@ -1,4 +1,4 @@
-package com.palmergames.bukkit.towny.event;
+package com.palmergames.bukkit.towny.event.mobs;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
@@ -7,17 +7,17 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class MobRemovalEvent extends Event implements Cancellable {
+public class MobSpawnRemovalEvent extends Event implements Cancellable {
 	private static final HandlerList handlers = new HandlerList();
 
 	private boolean cancelled = false;
 	private final Entity entity;
-	
-	public MobRemovalEvent(Entity entity) {
+
+	public MobSpawnRemovalEvent(Entity entity) {
 		super(!Bukkit.getServer().isPrimaryThread());
 		this.entity = entity;
 	}
-	
+
 	@Override
 	public HandlerList getHandlers() {
 		return handlers;
@@ -26,7 +26,7 @@ public class MobRemovalEvent extends Event implements Cancellable {
 	public static HandlerList getHandlerList() {
 		return handlers;
 	}
-	
+
 	@Override
 	public boolean isCancelled() {
 		return cancelled;
@@ -36,11 +36,11 @@ public class MobRemovalEvent extends Event implements Cancellable {
 	public void setCancelled(boolean isCancelled) {
 		cancelled = isCancelled;
 	}
-	
+
 	public Entity getEntity() {
 		return this.entity;
 	}
-	
+
 	public EntityType getEntityType() {
 		return this.entity.getType();
 	}
--- src/com/palmergames/bukkit/towny/listeners/TownyEntityListener.java
+++ src/com/palmergames/bukkit/towny/listeners/TownyEntityListener.java
@@ -5,7 +5,7 @@ import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.TownyMessaging;
 import com.palmergames.bukkit.towny.TownySettings;
 import com.palmergames.bukkit.towny.TownyUniverse;
-import com.palmergames.bukkit.towny.event.MobSpawnRemovalEvent;
+import com.palmergames.bukkit.towny.event.mobs.MobSpawnRemovalEvent;
 import com.palmergames.bukkit.towny.event.executors.TownyActionEventExecutor;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.exceptions.TownyException;
