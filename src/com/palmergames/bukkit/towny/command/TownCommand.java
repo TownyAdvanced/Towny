ecd31de0bb2f12bbb9be648ffcb7bb21649ac5cb
Llm Dl <LlmDlio@gmail.com>
2020-11-16 18:09:04 -0600

    - Add catches for NPEs when town.getAccount() returns null.

--- src/com/palmergames/bukkit/towny/command/TownCommand.java
+++ src/com/palmergames/bukkit/towny/command/TownCommand.java
@@ -2553,6 +2553,8 @@ public class TownCommand extends BaseCommand implements CommandExecutor, TabComp
 				town.getAccount().setBalance(0, "Setting 0 balance for Town");
 			} catch (EconomyException e) {
 				e.printStackTrace();
+			} catch (NullPointerException e1) {
+				throw new TownyException("The server economy plugin " + TownyEconomyHandler.getVersion() + " could not return the Town account!");
 			}
 		}
 		
@@ -3449,6 +3451,8 @@ public class TownCommand extends BaseCommand implements CommandExecutor, TabComp
 							throw new TownyException(Translation.of("msg_no_funds_claim2", selection.size(), TownyEconomyHandler.getFormattedBalance(blockCost),  TownyEconomyHandler.getFormattedBalance(missingAmount), new DecimalFormat("#").format(missingAmount)));
 					} catch (EconomyException e1) {
 						throw new TownyException("Economy Error");
+					} catch (NullPointerException e2) {
+						throw new TownyException("The server economy plugin " + TownyEconomyHandler.getVersion() + " could not return the Town account!");
 					}
 				}
 				new TownClaim(plugin, player, town, selection, outpost, true, false).start();
