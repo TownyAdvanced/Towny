0fff0fa64c88ad73ebc645bb649123f84b927fb5
Llm Dl <LlmDlio@gmail.com>
2020-11-22 08:44:31 -0600

    - Replace SiegeWarBreakBlockController with TownyDestroyEvent listener.

--- src/com/palmergames/bukkit/towny/war/siegewar/listeners/SiegeWarActionListener.java
+++ src/com/palmergames/bukkit/towny/war/siegewar/listeners/SiegeWarActionListener.java
@@ -1,8 +1,14 @@
 package com.palmergames.bukkit.towny.war.siegewar.listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.TownySettings;
+import com.palmergames.bukkit.towny.event.actions.TownyDestroyEvent;
+import com.palmergames.bukkit.towny.object.Translation;
+import com.palmergames.bukkit.towny.war.siegewar.utils.SiegeWarBlockUtil;
 
 public class SiegeWarActionListener implements Listener {
 
@@ -13,4 +19,16 @@ public class SiegeWarActionListener implements Listener {
 
 		plugin = instance;
 	}
+	
+	@EventHandler
+	public void onBlockBreak(TownyDestroyEvent event) {
+		if (TownySettings.getWarSiegeEnabled()) {
+			if (SiegeWarBlockUtil.isBlockNearAnActiveSiegeBanner(Bukkit.getWorld(event.getLocation().getWorld().getName()).getBlockAt(event.getLocation()))) {
+				event.setMessage(Translation.of("msg_err_siege_war_cannot_destroy_siege_banner"));
+				event.setCancelled(true);
+			}
+		}
+	}
+	
+	
 }
