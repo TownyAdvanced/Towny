42cd5ab59120d4797c39e32d1bf2836a6c0ad32e
FlagCourier <flagcourier@pm.me>
2020-11-25 06:07:29 -0600

    Smol clean & sticky pistons can't move immutable FlagWar blocks (#4469)
    - Ignore LSP server from RedHat's Dependency Analytics plugin
      - There's an IDEA plugin by RedHat that detects outdated and vulnerable dependencies. Without ignoring it, it adds a configuration file, and any logs created when maven is updated or ran from within IDEA.
    - Document the CellUnderAttack constructors & actually deprecate old one.
    - Remove old test code from TownyWarBlockListener
      - Hey, it said itself to remove it! It's as old as ElgarL's move to a GitHub repo...
    - Fix Random Javadoc warnings from elsewhere (missing params)
    - Prevent Pistons from Destroying FlagWar Beacons (FINALLY!)
      - Rather easy, since isSticky() exists. Not sure if it didn't when this code was originally written or not, though...

--- src/com/palmergames/bukkit/towny/war/flagwar/listeners/FlagWarBlockListener.java
+++ src/com/palmergames/bukkit/towny/war/flagwar/listeners/FlagWarBlockListener.java
@@ -1,7 +1,6 @@
 package com.palmergames.bukkit.towny.war.flagwar.listeners;
 
 import org.bukkit.block.Block;
-//import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,46 +8,10 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockPistonExtendEvent;
 import org.bukkit.event.block.BlockPistonRetractEvent;
-//import org.bukkit.event.block.BlockPlaceEvent;
 
-import com.palmergames.bukkit.towny.Towny;
 import com.palmergames.bukkit.towny.war.flagwar.FlagWar;
 
 public class FlagWarBlockListener implements Listener {
-
-	//private Towny plugin;
-
-	public FlagWarBlockListener(Towny plugin) {
-
-		//this.plugin = plugin;
-	}
-
-	/* TODO Remove This Commented Code
-	 * For Testing purposes only.
-	 *
-	 *
-	 * @EventHandler(priority = EventPriority.LOWEST)
-	 * public void onBlockPlace(BlockPlaceEvent event) {
-	 * Player player = event.getPlayer();
-	 * Block block = event.getBlockPlaced();
-	 * 
-	 * if (block == null)
-	 * return;
-	 * 
-	 * if (block.getType() == FlagWarConfig.getFlagBaseMaterial()) {
-	 * int topY = block.getWorld().getHighestBlockYAt(block.getX(),
-	 * block.getZ()) - 1;
-	 * if (block.getY() >= topY) {
-	 * CellAttackEvent cellAttackEvent = new CellAttackEvent(player, block);
-	 * this.plugin.getServer().getPluginManager().callEvent(cellAttackEvent);
-	 * if (cellAttackEvent.isCancelled()) {
-	 * event.setBuild(false);
-	 * event.setCancelled(true);
-	 * }
-	 * }
-	 * }
-	 * }
-	 */
 
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onBlockBreak(BlockBreakEvent event) {
@@ -70,9 +33,14 @@ public class FlagWarBlockListener implements Listener {
 	}
 
 	
-	// TODO: Need to check if a immutable block is being moved with a sticky piston.
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onBlockPistonRetract(BlockPistonRetractEvent event) {
+
+		if (event.isSticky()) {
+			
+			for (Block block : event.getBlocks())
+				FlagWar.checkBlock(null, block, event);
+		}
 
 	}
 }
