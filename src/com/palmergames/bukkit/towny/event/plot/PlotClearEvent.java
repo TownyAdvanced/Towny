4774b14092480d116c705d9cfdddcac80b3269f1
Kovalski <murattnt@live.com>
2020-12-15 20:18:57 +0300

    Implemented New Plot Events

---
+++ src/com/palmergames/bukkit/towny/event/plot/PlotClearEvent.java
@@ -0,0 +1,38 @@
+package com.palmergames.bukkit.towny.event.plot;
+
+import com.palmergames.bukkit.towny.object.TownBlock;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+
+public class PlotClearEvent extends Event {
+
+	private static final HandlerList handlers = new HandlerList();
+
+	private final TownBlock townBlock;
+
+	@Override
+	public HandlerList getHandlers() {
+
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+
+		return handlers;
+	}
+
+	public PlotClearEvent(TownBlock _townBlock) {
+		super(!Bukkit.getServer().isPrimaryThread());
+		this.townBlock = _townBlock;
+	}
+
+	/**
+	 * @return the new TownBlock.
+	 */
+	public TownBlock getTownBlock() {
+		return townBlock;
+	}
+
+}
