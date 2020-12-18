3b99a43db724a301ad8cdded0928dcb613cd2bfd
Llm Dl <LlmDlio@gmail.com>
2020-12-18 06:29:52 -0600

    Balance comparator uses new cached balance.

--- src/com/palmergames/bukkit/towny/object/comparators/GovernmentComparators.java
+++ src/com/palmergames/bukkit/towny/object/comparators/GovernmentComparators.java
@@ -1,7 +1,6 @@
 package com.palmergames.bukkit.towny.object.comparators;
 
 import com.palmergames.bukkit.towny.TownyAPI;
-import com.palmergames.bukkit.towny.exceptions.EconomyException;
 import com.palmergames.bukkit.towny.object.Government;
 import com.palmergames.bukkit.towny.object.TownyObject;
 
@@ -13,13 +12,7 @@ public class GovernmentComparators {
 
 	public static final Comparator<Government> BY_NUM_RESIDENTS = (t1, t2) -> t2.getResidents().size() - t1.getResidents().size();
 	public static final Comparator<Government> BY_NAME = Comparator.comparing(TownyObject::getName);
-	public static final Comparator<Government> BY_BANK_BALANCE = (g1, g2) -> {
-		try {
-			return Double.compare(g2.getAccount().getHoldingBalance(), g1.getAccount().getHoldingBalance());
-		} catch (EconomyException e) {
-			throw new RuntimeException("Failed to get balance. Aborting.");
-		}
-	};
+	public static final Comparator<Government> BY_BANK_BALANCE = (g1, g2) -> Double.compare(g2.getAccount().getCachedBalance(), g1.getAccount().getCachedBalance());
 	public static final Comparator<Government> BY_NUM_ONLINE = (g1, g2) -> TownyAPI.getInstance().getOnlinePlayers(g2).size() - TownyAPI.getInstance().getOnlinePlayers(g1).size();
 	public static final Comparator<Government> BY_TOWNBLOCKS_CLAIMED = (g1, g2) -> Double.compare(g2.getTownBlocks().size(), g1.getTownBlocks().size());
 	public static final Comparator<Government> BY_OPEN = (t1, t2) -> {
