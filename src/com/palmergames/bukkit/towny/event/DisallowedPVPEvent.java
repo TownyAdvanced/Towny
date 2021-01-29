885550cf92197dd49cd4866484eb1f374b7700da
Llm Dl <LlmDlio@gmail.com>
2021-01-29 10:10:50 -0600

    Update version in deprecated event.

--- src/com/palmergames/bukkit/towny/event/DisallowedPVPEvent.java
+++ src/com/palmergames/bukkit/towny/event/DisallowedPVPEvent.java
@@ -7,7 +7,7 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 /**
- * Deprecated as of 0.96.4.2. Use {@link com.palmergames.bukkit.towny.event.damage.TownyPlayerDamagePlayerEvent} instead.
+ * Deprecated as of 0.96.7.1. Use {@link com.palmergames.bukkit.towny.event.damage.TownyPlayerDamagePlayerEvent} instead.
  */
 @Deprecated
 public class DisallowedPVPEvent extends Event implements Cancellable {
