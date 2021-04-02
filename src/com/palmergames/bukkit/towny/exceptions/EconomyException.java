ab869b2a61736daa5c4cc5629f8fb7ed7e93b89e
Llm Dl <LlmDlio@gmail.com>
2021-04-02 11:37:01 -0500

    Mark EconomyException as deprecated, hopefully leading to its swift removal by those who've had to catch it in the past.

--- src/com/palmergames/bukkit/towny/exceptions/EconomyException.java
+++ src/com/palmergames/bukkit/towny/exceptions/EconomyException.java
@@ -1,16 +1,28 @@
 package com.palmergames.bukkit.towny.exceptions;
 
+/**
+ * @deprecated since 0.96.7.12 - Towny no longer throws this Exception internally.
+ */
+@Deprecated
 public class EconomyException extends Exception {
 
 	private static final long serialVersionUID = 5273714478509976170L;
 	public String error;
 
+	/**
+	 * @deprecated since 0.96.7.12 - Towny no longer throws this Exception internally.
+	 */
+	@Deprecated
 	public EconomyException() {
 
 		super();
 		error = "unknown";
 	}
 
+	/**
+	 * @deprecated since 0.96.7.12 - Towny no longer throws this Exception internally.
+	 */
+	@Deprecated
 	public EconomyException(String error) {
 
 		super(error);
