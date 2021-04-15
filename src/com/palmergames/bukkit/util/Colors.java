d31ad3ec3cc853f7021f734d9e904aa8fdcafc8a
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-04-15 02:03:36 +0200

    Add toNamedTextColor function

--- src/com/palmergames/bukkit/util/Colors.java
+++ src/com/palmergames/bukkit/util/Colors.java
@@ -1,6 +1,9 @@
 package com.palmergames.bukkit.util;
 
 import org.bukkit.ChatColor;
+import org.jetbrains.annotations.Nullable;
+
+import net.kyori.adventure.text.format.NamedTextColor;
 
 public class Colors {
 
@@ -30,5 +33,49 @@ public class Colors {
 
 	public static String translateColorCodes(String str) {
 		return ChatColor.translateAlternateColorCodes('&', str);
+	}
+
+	/**
+	 * @param color The color code.
+	 * @return the {@link NamedTextColor} for the entered color string, or null if it is invalid.
+	 */
+	@Nullable
+	public static NamedTextColor toNamedTextColor(String colorCode) {
+		switch(colorCode) {
+			case "\u00A70":
+				return NamedTextColor.BLACK;
+			case "\u00A71":
+				return NamedTextColor.DARK_BLUE;
+			case "\u00A72":
+				return NamedTextColor.DARK_GREEN;
+			case "\u00A73":
+				return NamedTextColor.DARK_AQUA;
+			case "\u00A74":
+				return NamedTextColor.DARK_RED;
+			case "\u00A75":
+				return NamedTextColor.DARK_PURPLE;
+			case "\u00A76":
+				return NamedTextColor.GOLD;
+			case "\u00A77":
+				return NamedTextColor.GRAY;
+			case "\u00A78":
+				return NamedTextColor.DARK_GRAY;
+			case "\u00A79":
+				return NamedTextColor.BLUE;
+			case "\u00A7a":
+				return NamedTextColor.GREEN;
+			case "\u00A7b":
+				return NamedTextColor.AQUA;
+			case "\u00A7c":
+				return NamedTextColor.RED;
+			case "\u00A7d":
+				return NamedTextColor.LIGHT_PURPLE;
+			case "\u00A7e":
+				return NamedTextColor.YELLOW;
+			case "\u00A7f":
+				return NamedTextColor.WHITE;
+			default:
+				return null;
+		}
 	}
 }
