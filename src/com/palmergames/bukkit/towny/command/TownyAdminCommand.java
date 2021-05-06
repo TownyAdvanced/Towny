8313c5c1b41fd9e0e2e25aa05fcc162bd3964db7
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-05-06 20:49:17 +0200

    Unused import

--- src/com/palmergames/bukkit/towny/command/TownyAdminCommand.java
+++ src/com/palmergames/bukkit/towny/command/TownyAdminCommand.java
@@ -55,7 +55,6 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
