7ea10ae40ec95f4de1be21c2d9b248536fa4eecb
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-08-03 23:03:05 +0200

    Add registry loading code

--- src/com/palmergames/bukkit/towny/object/Translation.java
+++ src/com/palmergames/bukkit/towny/object/Translation.java
@@ -8,12 +8,23 @@ import com.palmergames.bukkit.towny.TownySettings;
 import com.palmergames.bukkit.towny.command.HelpMenu;
 import com.palmergames.bukkit.util.Colors;
 import com.palmergames.util.FileMgmt;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.translation.GlobalTranslator;
+import net.kyori.adventure.translation.TranslationRegistry;
 import org.bukkit.configuration.InvalidConfigurationException;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.SafeConstructor;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * A convenience object to facilitate translation. 
@@ -21,7 +32,42 @@ import java.util.Map;
 public final class Translation {
 	
 	public static CommentedConfiguration language;
-	private static Map<String, String> oldLangFileNames = createLegacyLangMap();
+	private static final Map<String, String> oldLangFileNames = createLegacyLangMap();
+	private static final Set<String> langFiles = new HashSet<>(Arrays.asList("da-DK", "de-DE", "en-US", "es-419", "es-ES", "fr-FR", "id-ID", "it-IT", "ko-KR", "nl-NL", "no-NO", "pl-PL", "pt-BR", "ru-RU", "sv-SE", "tr-TR", "zh-CN", "zh-TW"));
+	private static TranslationRegistry registry;
+	
+	public static void loadTranslationRegistry() {
+		if (registry != null)
+			GlobalTranslator.get().removeSource(registry);
+		
+		registry = TranslationRegistry.create(Key.key("towny", "main"));		
+		registry.defaultLocale(toLocale(TownySettings.getString(ConfigNodes.LANGUAGE)));
+		
+		for (String lang : langFiles) {
+			try (InputStream is = Translation.class.getResourceAsStream("/lang/" + lang + ".yml")) {
+				Map<String, Object> values = new Yaml(new SafeConstructor()).load(is);
+				Locale locale = toLocale(lang);
+				
+				for (Map.Entry<String, Object> entry : values.entrySet()) {
+					if (!(entry.getValue() instanceof String))
+						continue;
+					
+					try {
+						registry.register(entry.getKey(), locale, new MessageFormat((String) entry.getValue()));
+					} catch (IllegalArgumentException e) {
+						Towny.getPlugin().getLogger().warning("The string '" + entry.getValue() + "' is not a valid MessageFormat. Key: " + entry.getKey() + " | Locale: " + locale);
+					}
+				}
+			} catch (Exception e) {
+				// An IO exception occured, or the file had invalid yaml
+				e.printStackTrace();
+			}
+		}
+		
+		//TODO: allow languages to be added/overridden
+		
+		GlobalTranslator.get().addSource(registry);
+	}
 
 	// This will read the language entry in the config.yml to attempt to load
 	// custom languages
@@ -111,7 +157,7 @@ public final class Translation {
 	}
 
 	private static Map<String, String> createLegacyLangMap() {
-		Map<String, String> oldLangFileNames = new HashMap<String, String>();
+		Map<String, String> oldLangFileNames = new HashMap<>();
 		oldLangFileNames.put("danish.yml", "da-DK.yml");
 		oldLangFileNames.put("german.yml", "de-DE.yml");
 		oldLangFileNames.put("english.yml", "en-US.yml");
@@ -126,5 +172,15 @@ public final class Translation {
 		oldLangFileNames.put("sv-SE.yml", "sv-SE.yml");
 		oldLangFileNames.put("chinese.yml", "zh-CN.yml");
 		return oldLangFileNames;
+	}
+	
+	private static Locale toLocale(String fileName) {
+		int lastIndex = fileName.lastIndexOf(".") == -1 ? fileName.length() : fileName.lastIndexOf(".");
+		try {
+			String[] locale = fileName.substring(0, lastIndex).split("-");
+			return new Locale(locale[0], locale[1]);
+		} catch (Exception e) {
+			return Locale.ENGLISH;
+		}
 	}
 }
