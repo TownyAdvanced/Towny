b90895c30d82d8985f1f0440cba46bcc287c8b73
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-08-13 22:44:36 +0200

    Finish translation loading code

--- src/com/palmergames/bukkit/towny/TownyUniverse.java
+++ src/com/palmergames/bukkit/towny/TownyUniverse.java
@@ -192,7 +192,6 @@ public class TownyUniverse {
             DatabaseConfig.loadDatabaseConfig(rootFolder + File.separator + "settings" + File.separator + "database.yml");
             TownySettings.loadConfig(rootFolder + File.separator + "settings" + File.separator + "config.yml", towny.getVersion());
 			Translation.loadTranslationRegistry();
-			Translation.loadLanguage(rootFolder + File.separator + "settings", "en-US.yml");
             TownyPerms.loadPerms(rootFolder + File.separator + "settings", "townyperms.yml");
         } catch (IOException | TownyException e) {
             e.printStackTrace();
