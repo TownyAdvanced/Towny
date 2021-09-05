b0ae3136beec50a25dfa96070e70612da9ef9a91
Llm Dl <LlmDlio@gmail.com>
2021-09-05 15:54:17 -0500

    - Raise language level to 16 using jabel, courtesy of Warriorrrr with PR #5259.

--- src/com/palmergames/bukkit/towny/exceptions/MetaDataSerializationException.java
+++ src/com/palmergames/bukkit/towny/exceptions/MetaDataSerializationException.java
@@ -1,7 +1,5 @@
 package com.palmergames.bukkit.towny.exceptions;
 
-import com.palmergames.bukkit.towny.exceptions.TownyException;
-
 public class MetaDataSerializationException extends TownyException {
 	private static final long serialVersionUID = -5272182994988841260L;
     
