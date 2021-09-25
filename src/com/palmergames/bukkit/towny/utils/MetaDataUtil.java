5a366585d153fd31cf08e4be5ca1864ad1e47bc0
Llm Dl <LlmDlio@gmail.com>
2021-09-25 07:17:15 -0500

    Comments for the MetaDataUtil.

--- src/com/palmergames/bukkit/towny/utils/MetaDataUtil.java
+++ src/com/palmergames/bukkit/towny/utils/MetaDataUtil.java
@@ -15,6 +15,13 @@ import com.palmergames.bukkit.towny.object.metadata.StringDataField;
  */
 public class MetaDataUtil {
 
+	/**
+	 * Get a string from a TownyObject's metadata.
+	 * 
+	 * @param townyObject TownyObject, ie: Resident, Town, Nation, TownBlock.
+	 * @param sdf StringDataField to get from the TownyObject.
+	 * @return String from the metadata or an empty string.
+	 */
 	public static String getString(TownyObject townyObject, StringDataField sdf) {
 		CustomDataField<?> cdf = townyObject.getMetadata(sdf.getKey());
 		if (cdf instanceof StringDataField) {
@@ -23,6 +30,13 @@ public class MetaDataUtil {
 		return "";
 	}
 
+	/**
+	 * Get a boolean from a TownyObject's metadata.
+	 * 
+	 * @param townyObject TownyObject, ie: Resident, Town, Nation, TownBlock.
+	 * @param bdf BooleanDataField to get from the TownyObject.
+	 * @return boolean from the metadata or false.
+	 */
 	public static boolean getBoolean(TownyObject townyObject, BooleanDataField bdf) {
 		CustomDataField<?> cdf = townyObject.getMetadata(bdf.getKey());
 		if (cdf instanceof BooleanDataField)
@@ -30,6 +44,13 @@ public class MetaDataUtil {
 		return false;
 	}
 
+	/**
+	 * Get a long from a TownyObject's metadata.
+	 * 
+	 * @param townyObject TownyObject, ie: Resident, Town, Nation, TownBlock.
+	 * @param ldf LongDataField to get from the TownyObject.
+	 * @return long from the metadata or 0l.
+	 */
 	public static long getLong(TownyObject townyObject, LongDataField ldf) {
 		CustomDataField<?> cdf = townyObject.getMetadata(ldf.getKey());
 		if (cdf instanceof LongDataField)
@@ -37,6 +58,13 @@ public class MetaDataUtil {
 		return 0l;
 	}
 	
+	/**
+	 * Get a int from a TownyObject's metadata.
+	 * 
+	 * @param townyObject TownyObject, ie: Resident, Town, Nation, TownBlock.
+	 * @param idf IntegerDataField to get from the TownyObject.
+	 * @return int from the metadata or 0.
+	 */
 	public static int getInt(TownyObject townyObject, IntegerDataField idf) {
 		CustomDataField<?> cdf = townyObject.getMetadata(idf.getKey());
 		if (cdf instanceof IntegerDataField) 
@@ -44,6 +72,13 @@ public class MetaDataUtil {
 		return 0;				
 	}
 	
+	/**
+	 * Get a double from a TownyObject's metadata.
+	 * 
+	 * @param townyObject TownyObject, ie: Resident, Town, Nation, TownBlock.
+	 * @param ddf DecimalDataField to get from the TownyObject.
+	 * @return double from the metadata or 0.0.
+	 */
 	public static double getDouble(TownyObject townyObject, DecimalDataField ddf) {
 		CustomDataField<?> cdf = townyObject.getMetadata(ddf.getKey());
 		if (cdf instanceof DecimalDataField)
@@ -51,48 +86,93 @@ public class MetaDataUtil {
 		return 0.0;
 	}
 
-	public static void setString(TownyObject townyObject, StringDataField sdf, String string) {
+	/**
+	 * Sets a StringDataField metadata on a TownyObject.
+	 * 
+	 * @param townyObject TownyObject, ie: Resident, Town, Nation, TownBlock.
+	 * @param sdf StringDataField to apply to the TownyObject.
+	 * @param string String to apply to the StringDataField.
+	 * @param save True to save the TownyObject after setting the metadata. 
+	 */
+	public static void setString(TownyObject townyObject, StringDataField sdf, String string, boolean save) {
 		CustomDataField<?> cdf = townyObject.getMetadata(sdf.getKey());
 		if (cdf instanceof StringDataField) {
 			StringDataField value = (StringDataField) cdf;
 			value.setValue(string);
-			townyObject.save();
+			if (save)
+				townyObject.save();
 		}
 	}
 
-	public static void setBoolean(TownyObject townyObject, BooleanDataField bdf, boolean bool) {
+	/**
+	 * Sets a BooleanDataField metadata on a TownyObject.
+	 * 
+	 * @param townyObject TownyObject, ie: Resident, Town, Nation, TownBlock.
+	 * @param bdf BooleanDataField to apply to the TownyObject.
+	 * @param bool boolean to apply to the BooleanDataField.
+	 * @param save True to save the TownyObject after setting the metadata. 
+	 */
+	public static void setBoolean(TownyObject townyObject, BooleanDataField bdf, boolean bool, boolean save) {
 		CustomDataField<?> cdf = townyObject.getMetadata(bdf.getKey());
 		if (cdf instanceof BooleanDataField) {
 			BooleanDataField value = (BooleanDataField) cdf;
 			value.setValue(bool);
-			townyObject.save();
+			if (save)
+				townyObject.save();
 		}
 	}
 
-	public static void setLong(TownyObject townyObject, LongDataField ldf, long num) {
+	/**
+	 * Sets a LongDataField metadata on a TownyObject.
+	 * 
+	 * @param townyObject TownyObject, ie: Resident, Town, Nation, TownBlock.
+	 * @param ldf LongDataField to apply to the TownyObject.
+	 * @param num long to apply to the LongDataField.
+	 * @param save True to save the TownyObject after setting the metadata. 
+	 */
+	public static void setLong(TownyObject townyObject, LongDataField ldf, long num, boolean save) {
 		CustomDataField<?> cdf = townyObject.getMetadata(ldf.getKey());
 		if (cdf instanceof LongDataField) {
 			LongDataField value = (LongDataField) cdf;
 			value.setValue(num);
-			townyObject.save();
+			if (save)
+				townyObject.save();
 		}
 	}
 
-	public static void setInt(TownyObject townyObject, IntegerDataField idf, int num) {
+	/**
+	 * Sets a IntegerDataField metadata on a TownyObject.
+	 * 
+	 * @param townyObject TownyObject, ie: Resident, Town, Nation, TownBlock.
+	 * @param idf IntegerDataField to apply to the TownyObject.
+	 * @param num int to apply to the IntegerDataField.
+	 * @param save True to save the TownyObject after setting the metadata. 
+	 */
+	public static void setInt(TownyObject townyObject, IntegerDataField idf, int num, boolean save) {
 		CustomDataField<?> cdf = townyObject.getMetadata(idf.getKey());
 		if (cdf instanceof IntegerDataField) {
 			IntegerDataField value = (IntegerDataField) cdf;
 			value.setValue(num);
-			townyObject.save();
+			if (save)
+				townyObject.save();
 		}
 	}
 	
-	public static void setDouble(TownyObject townyObject, DecimalDataField ddf, double num) {
+	/**
+	 * Sets a DecimalDataField metadata on a TownyObject.
+	 * 
+	 * @param townyObject TownyObject, ie: Resident, Town, Nation, TownBlock.
+	 * @param ddf DecimalDataField to apply to the TownyObject.
+	 * @param num double to apply to the DecimalDataField.
+	 * @param save True to save the TownyObject after setting the metadata. 
+	 */
+	public static void setDouble(TownyObject townyObject, DecimalDataField ddf, double num, boolean save) {
 		CustomDataField<?> cdf = townyObject.getMetadata(ddf.getKey());
 		if (cdf instanceof DecimalDataField) {
 			DecimalDataField value = (DecimalDataField) cdf;
 			value.setValue(num);
-			townyObject.save();
+			if (save)
+				townyObject.save();
 		}
 	}
 }
