3433e5cdbd751e0cda4d90e310359d6314b3161a
Gabriel <65878291+GNosii@users.noreply.github.com>
2021-10-07 21:19:40 -0300

    Apply requested change

--- src/com/palmergames/bukkit/towny/listeners/TownyTownBlockTypeListener.java
+++ src/com/palmergames/bukkit/towny/listeners/TownyTownBlockTypeListener.java
@@ -1,9 +1,15 @@
 package com.palmergames.bukkit.towny.listeners;
 
 import com.palmergames.bukkit.towny.TownyAPI;
-import com.palmergames.bukkit.towny.event.actions.*;
-import com.palmergames.bukkit.towny.event.damage.TownyDamageEvent;
-import com.palmergames.bukkit.towny.object.TownBlock;
+import com.palmergames.bukkit.towny.event.PlotChangeTypeEvent;
+import com.palmergames.bukkit.towny.event.actions.TownyBuildEvent;
+import com.palmergames.bukkit.towny.event.actions.TownyDestroyEvent;
+import com.palmergames.bukkit.towny.event.actions.TownySwitchEvent;
+import com.palmergames.bukkit.towny.event.actions.TownyItemuseEvent;
+import com.palmergames.bukkit.towny.event.actions.TownyBurnEvent;
+import com.palmergames.bukkit.towny.event.actions.TownyExplodingBlocksEvent;
+import com.palmergames.bukkit.towny.event.damage.TownBlockPVPTestEvent;
+import com.palmergames.bukkit.towny.object.*;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,6 +21,12 @@ import org.bukkit.event.Listener;
  */
 public class TownyTownBlockTypeListener implements Listener {
 
+	@EventHandler
+	public void onTownyCustomTownBlockType(PlotChangeTypeEvent event) {
+		if (event.getNewType() == TownBlockType.CUSTOM)
+			event.getTownBlock().setPlotPrice(event.getTownBlock().getCustomTownBlockType().getPrice());
+	}
+	
 	@EventHandler
 	public void onTownyBuild(TownyBuildEvent event) {
 		if (!event.isInWilderness()) {
@@ -28,7 +40,7 @@ public class TownyTownBlockTypeListener implements Listener {
 	public void onTownyDestroy(TownyDestroyEvent event) {
 		if (!event.isInWilderness()) {
 			TownBlock tb = event.getTownBlock();
-			if (tb != null && !tb.hasCustomTownBlockType())
+			if (tb != null && tb.hasCustomTownBlockType())
 				event.setCancelled(!tb.getCustomTypeHandler().onTownyDestroy(tb));
 		}
 	}
@@ -37,7 +49,7 @@ public class TownyTownBlockTypeListener implements Listener {
 	public void onTownySwitch(TownySwitchEvent event) {
 		if (!event.isInWilderness()) {
 			TownBlock tb = event.getTownBlock();
-			if (tb != null && !tb.hasCustomTownBlockType())
+			if (tb != null && tb.hasCustomTownBlockType())
 				event.setCancelled(!tb.getCustomTypeHandler().onTownySwitch(tb));
 		}
 	}
@@ -46,7 +58,7 @@ public class TownyTownBlockTypeListener implements Listener {
 	public void onTownyItemUse(TownyItemuseEvent event) {
 		if (!event.isInWilderness()) {
 			TownBlock tb = event.getTownBlock();
-			if (tb != null && !tb.hasCustomTownBlockType())
+			if (tb != null && tb.hasCustomTownBlockType())
 				event.setCancelled(!tb.getCustomTypeHandler().onTownyItemUse(tb));
 		}
 	}
@@ -55,7 +67,7 @@ public class TownyTownBlockTypeListener implements Listener {
 	public void onTownyBurn(TownyBurnEvent event) {
 		if (!event.isInWilderness()) {
 			TownBlock tb = event.getTownBlock();
-			if (tb != null && !tb.hasCustomTownBlockType())
+			if (tb != null && tb.hasCustomTownBlockType())
 				event.setCancelled(!tb.getCustomTypeHandler().onTownyBurn(tb));
 		}
 	}
@@ -65,18 +77,16 @@ public class TownyTownBlockTypeListener implements Listener {
 		Block testBlock = event.getBlockList().get(0);
 		if (!TownyAPI.getInstance().isWilderness(testBlock)) {
 			TownBlock tb = TownyAPI.getInstance().getTownBlock(testBlock.getLocation());
-			if (tb != null && !tb.hasCustomTownBlockType())
+			if (tb != null && tb.hasCustomTownBlockType())
 				if (!tb.getCustomTypeHandler().onTownyExplosion(tb))
 					event.getTownyFilteredBlockList().clear();
 		}
 	}
 
 	@EventHandler
-	public void onTownyDamage(TownyDamageEvent event) {
-		if (!event.isInWilderness()) {
-			TownBlock tb = event.getTownBlock();
-			if (tb != null && !tb.hasCustomTownBlockType())
-				event.setCancelled(!tb.getCustomTypeHandler().onTownyDamage(tb));
-		}
+	public void onTownyDamage(TownBlockPVPTestEvent event) {
+		TownBlock tb = event.getTownBlock();
+		if (tb != null && !tb.hasCustomTownBlockType())
+				event.setPvp(tb.getCustomTypeHandler().onTownyDamage(tb));
 	}
 }
