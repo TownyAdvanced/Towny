ec39a21f4310dfa04bd8232335b7137d8ec2e9d3
Llm Dl <LlmDlio@gmail.com>
2021-11-03 15:24:14 -0500

    - Add closed_economy_enabled bstats chart.

--- src/com/palmergames/bukkit/towny/Towny.java
+++ src/com/palmergames/bukkit/towny/Towny.java
@@ -1072,6 +1072,8 @@ public class Towny extends JavaPlugin {
 		
 		metrics.addCustomChart(new SimplePie("town_block_size", () -> String.valueOf(TownySettings.getTownBlockSize())));
 		
+		metrics.addCustomChart(new SimplePie("closed_economy_enabled", () -> String.valueOf(TownySettings.isEcoClosedEconomyEnabled())));
+		
 		metrics.addCustomChart(new SimplePie("resident_uuids_stored", TownySettings::getUUIDPercent));
 	}
 	
