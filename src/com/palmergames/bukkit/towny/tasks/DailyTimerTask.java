8309484c6ba91b261c2d70eb421f2f5ea4a11806
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-12-05 23:20:16 +0100

    Fix tax collection in the DailyTimerTask

--- src/com/palmergames/bukkit/towny/tasks/DailyTimerTask.java
+++ src/com/palmergames/bukkit/towny/tasks/DailyTimerTask.java
@@ -361,65 +361,60 @@ public class DailyTimerTask extends TownyTimerTask {
 		}
 
 		// Plot Tax
-		if (town.getPlotTax() > 0 || town.getCommercialPlotTax() > 0 || town.getEmbassyPlotTax() > 0) {
+		List<TownBlock> townBlocks = new ArrayList<>(town.getTownBlocks());
+		List<String> lostPlots = new ArrayList<>();
+		ListIterator<TownBlock> townBlockItr = townBlocks.listIterator();
+		TownBlock townBlock;
 
-			List<TownBlock> townBlocks = new ArrayList<>(town.getTownBlocks());
-			List<String> lostPlots = new ArrayList<>();
-			ListIterator<TownBlock> townBlockItr = townBlocks.listIterator();
-			TownBlock townBlock;
+		while (townBlockItr.hasNext()) {
+			townBlock = townBlockItr.next();
+			double tax = townBlock.getType().getTax(town);
 
-			while (townBlockItr.hasNext()) {
-				townBlock = townBlockItr.next();
+			if (!townBlock.hasResident() || tax < 1)
+				continue;
 
-				if (!townBlock.hasResident())
-					continue;
-				Resident resident = townBlock.getResidentOrNull();
+			Resident resident = townBlock.getResidentOrNull();
 
-				/*
-				 * Only collect plot tax from this resident if it really
-				 * still exists. We are running in an Async thread so MUST
-				 * verify all objects.
-				 */
-				if (universe.hasResident(resident.getName())) {
-					if (resident.hasTown() && resident.getTownOrNull() == town)
-						if (TownyPerms.getResidentPerms(resident).containsKey("towny.tax_exempt") || resident.isNPC())
-							continue;
-					
-					double tax = townBlock.getType().getTax(town);
-					if (tax < 1)
+			/*
+			 * Only collect plot tax from this resident if it really
+			 * still exists. We are running in an Async thread so MUST
+			 * verify all objects.
+			 */
+			if (universe.hasResident(resident.getName())) {
+				if (resident.hasTown() && resident.getTownOrNull() == town)
+					if (TownyPerms.getResidentPerms(resident).containsKey("towny.tax_exempt") || resident.isNPC())
 						continue;
 
-					// If the tax would put the town over the bank cap we reduce what will be
-					// paid by the plot owner to what will be allowed.
-					if (TownySettings.getTownBankCap() != 0 && tax + town.getAccount().getHoldingBalance() > TownySettings.getTownBankCap())
-						tax = town.getAccount().getBalanceCap() - town.getAccount().getHoldingBalance();
-					
-					if (!resident.getAccount().payTo(tax, town, String.format("Plot Tax (%s)", townBlock.getType()))) {
-						if (!lostPlots.contains(resident.getName()))
-							lostPlots.add(resident.getName());
+				// If the tax would put the town over the bank cap we reduce what will be
+				// paid by the plot owner to what will be allowed.
+				if (TownySettings.getTownBankCap() != 0 && tax + town.getAccount().getHoldingBalance() > TownySettings.getTownBankCap())
+					tax = town.getAccount().getBalanceCap() - town.getAccount().getHoldingBalance();
 
-						townBlock.setResident(null);
-						
-						// Set the plot price.
-						if (TownySettings.doesPlotTaxNonPaymentSetPlotForSale())
-							townBlock.setPlotPrice(town.getPlotTypePrice(townBlock.getType()));
-						else 
-							townBlock.setPlotPrice(-1);								
+				if (!resident.getAccount().payTo(tax, town, String.format("Plot Tax (%s)", townBlock.getType()))) {
+					if (!lostPlots.contains(resident.getName()))
+						lostPlots.add(resident.getName());
 
-						// Set the plot permissions to mirror the towns.
-						townBlock.setType(townBlock.getType());							
-						
-						townBlock.save();
-					}
+					townBlock.setResident(null);
+
+					// Set the plot price.
+					if (TownySettings.doesPlotTaxNonPaymentSetPlotForSale())
+						townBlock.setPlotPrice(town.getPlotTypePrice(townBlock.getType()));
+					else
+						townBlock.setPlotPrice(-1);
+
+					// Set the plot permissions to mirror the towns.
+					townBlock.setType(townBlock.getType());
+
+					townBlock.save();
 				}
-				
 			}
-			if (lostPlots != null && !lostPlots.isEmpty()) {
-				if (lostPlots.size() == 1) 
-					TownyMessaging.sendPrefixedTownMessage(town, Translatable.of("msg_couldnt_pay_plot_taxes", lostPlots.get(0)));
-				else
-					TownyMessaging.sendPrefixedTownMessage(town, Translatable.of("msg_couldnt_pay_plot_taxes_multiple").append(StringMgmt.join(lostPlots, ", ")));
-			}
+		}
+
+		if (lostPlots != null && !lostPlots.isEmpty()) {
+			if (lostPlots.size() == 1) 
+				TownyMessaging.sendPrefixedTownMessage(town, Translatable.of("msg_couldnt_pay_plot_taxes", lostPlots.get(0)));
+			else
+				TownyMessaging.sendPrefixedTownMessage(town, Translatable.of("msg_couldnt_pay_plot_taxes_multiple").append(StringMgmt.join(lostPlots, ", ")));
 		}
 	}
 
