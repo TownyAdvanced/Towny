27b49aab43bd8c5ec831b4a5391640595abb78bf
Ben Myers <41796709+benlmyers@users.noreply.github.com>
2022-01-08 09:22:52 -0800

    Add PAPI placeholder for town public/open status (#5602)
    * Add PAPI placeholder for town public/open status
    
    * Use translation of `status_title_open` for PAPI open town status

--- src/com/palmergames/bukkit/towny/TownyPlaceholderExpansion.java
+++ src/com/palmergames/bukkit/towny/TownyPlaceholderExpansion.java
@@ -585,6 +585,10 @@ public class TownyPlaceholderExpansion extends PlaceholderExpansion implements R
 			return resident.hasNation() ? (resident.getNationOrNull().isNeutral() ? Translation.of("status_town_title_peaceful"): "") : "";
 		case "is_town_peaceful": // %townyadvanced_is_town_peaceful%	
 			return resident.hasTown() ? (resident.getTownOrNull().isNeutral() ? Translation.of("status_town_title_peaceful"): "") : "";
+		case "is_town_public": // %townyadvanced_is_town_public%
+			return resident.hasTown() ? (resident.getTownOrNull().isPublic() ? Translation.of("status_public") : "") : "";
+		case "is_town_open": // %townyadvanced_is_town_open%
+			return resident.hasTown() ? (resident.getTownOrNull().isOpen() ? Translation.of("status_title_open") : "") : "";
 		case "town_board": // %townyadvanced_town_board%
 			return resident.hasTown() ? resident.getTownOrNull().getBoard() : "";
 		case "nation_board": // %townyadvanced_nation_board%
