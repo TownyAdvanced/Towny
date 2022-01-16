49f82311fac648be3e79772eaf4aa05d7ce7f914
Llm Dl <LlmDlio@gmail.com>
2022-01-16 10:21:01 -0600

    - Fix Town add/remove enemy/ally events being able to be thrown async.

--- src/com/palmergames/bukkit/towny/event/town/TownAddEnemiedTownEvent.java
+++ src/com/palmergames/bukkit/towny/event/town/TownAddEnemiedTownEvent.java
@@ -1,5 +1,6 @@
 package com.palmergames.bukkit.towny.event.town;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
@@ -15,6 +16,7 @@ public class TownAddEnemiedTownEvent extends Event implements Cancellable {
 	private String cancelMessage = "A town enemying another town was cancelled by another plugin.";
 	
 	public TownAddEnemiedTownEvent(Town town, Town newAlly) {
+		super(!Bukkit.getServer().isPrimaryThread());
 		this.town = town;
 		this.newEnemy = newAlly;
 	}
--- src/com/palmergames/bukkit/towny/event/town/TownRemoveEnemiedTownEvent.java
+++ src/com/palmergames/bukkit/towny/event/town/TownRemoveEnemiedTownEvent.java
@@ -1,5 +1,6 @@
 package com.palmergames.bukkit.towny.event.town;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
@@ -15,6 +16,7 @@ public class TownRemoveEnemiedTownEvent extends Event implements Cancellable {
 	private String cancelMessage = "A town removing an another town as an enemy was cancelled by another plugin.";
 	
 	public TownRemoveEnemiedTownEvent(Town town, Town newAlly) {
+		super(!Bukkit.getServer().isPrimaryThread());
 		this.town = town;
 		this.removedEnemy = newAlly;
 	}
