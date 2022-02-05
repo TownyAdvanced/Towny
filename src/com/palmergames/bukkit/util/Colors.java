87e28c2ee310245ba85c952bc6297ae49937893e
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2022-02-06 00:57:04 +0100

    Commit some work in progress code.

--- src/com/palmergames/bukkit/util/Colors.java
+++ src/com/palmergames/bukkit/util/Colors.java
@@ -3,6 +3,7 @@ package com.palmergames.bukkit.util;
 import com.palmergames.bukkit.towny.utils.TownyComponents;
 import net.kyori.adventure.text.Component;
 import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import com.palmergames.bukkit.towny.TownySettings;
@@ -12,24 +13,75 @@ import com.palmergames.util.StringMgmt;
 
 import net.kyori.adventure.text.format.NamedTextColor;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Colors {
 
-	public static final String Black = "\u00A70";
-	public static final String Navy = "\u00A71";
-	public static final String Green = "\u00A72";
-	public static final String Blue = "\u00A73";
-	public static final String Red = "\u00A74";
-	public static final String Purple = "\u00A75";
-	public static final String Gold = "\u00A76";
-	public static final String LightGray = "\u00A77";
-	public static final String Gray = "\u00A78";
-	public static final String DarkPurple = "\u00A79";
-	public static final String LightGreen = "\u00A7a";
-	public static final String LightBlue = "\u00A7b";
-	public static final String Rose = "\u00A7c";
-	public static final String LightPurple = "\u00A7d";
-	public static final String Yellow = "\u00A7e";
-	public static final String White = "\u00A7f";
+	public static final @Deprecated String Black = "\u00A70"; // -> black
+	public static final @Deprecated String Navy = "\u00A71"; // -> dark_blue
+	public static final @Deprecated String Green = "\u00A72"; // -> dark_green
+	public static final @Deprecated String Blue = "\u00A73"; // -> dark_aqua
+	public static final @Deprecated String Red = "\u00A74"; // -> dark_red
+	public static final @Deprecated String Purple = "\u00A75"; // -> dark_purple
+	public static final @Deprecated String Gold = "\u00A76"; // -> gold
+	public static final @Deprecated String LightGray = "\u00A77"; // -> gray
+	public static final @Deprecated String Gray = "\u00A78"; // -> dark_gray
+	public static final @Deprecated String DarkPurple = "\u00A79"; // -> blue
+	public static final @Deprecated String LightGreen = "\u00A7a"; // -> green
+	public static final @Deprecated String LightBlue = "\u00A7b"; // -> aqua
+	public static final @Deprecated String Rose = "\u00A7c"; // -> red
+	public static final @Deprecated String LightPurple = "\u00A7d"; // -> light_purple
+	public static final @Deprecated String Yellow = "\u00A7e"; // -> yellow
+	public static final @Deprecated String White = "\u00A7f"; // -> white
+	
+	public static final String DARK_RED = "<dark_red>";
+	public static final String RED = "<red>";
+	public static final String GOLD = "<gold>";
+	public static final String YELLOW = "<yellow>";
+	public static final String DARK_GREEN = "<dark_green>";
+	public static final String GREEN = "<green>";
+	public static final String DARK_AQUA = "<dark_aqua>";
+	public static final String AQUA = "<aqua>";
+	public static final String DARK_BLUE = "<dark_blue>";
+	public static final String BLUE = "<blue>";
+	public static final String LIGHT_PURPLE = "<light_purple>";
+	public static final String DARK_PURPLE = "<dark_purple>";
+	public static final String WHITE = "<white>";
+	public static final String GRAY = "<gray>";
+	public static final String DARK_GRAY = "<dark_gray>";
+	public static final String BLACK = "<black>";
+
+	private static final Map<String, String> legacyLookupMap = new HashMap<>(22);
+	private static final Pattern legacyPattern = Pattern.compile("[§&][0-9a-fk-or]");
+
+	static {
+		legacyLookupMap.put("4", DARK_RED);
+		legacyLookupMap.put("c", RED);
+		legacyLookupMap.put("6", GOLD);
+		legacyLookupMap.put("e", YELLOW);
+		legacyLookupMap.put("2", DARK_GREEN);
+		legacyLookupMap.put("a", GREEN);
+		legacyLookupMap.put("3", DARK_AQUA);
+		legacyLookupMap.put("b", AQUA);
+		legacyLookupMap.put("1", DARK_BLUE);
+		legacyLookupMap.put("9", BLUE);
+		legacyLookupMap.put("d", LIGHT_PURPLE);
+		legacyLookupMap.put("5", DARK_PURPLE);
+		legacyLookupMap.put("f", WHITE);
+		legacyLookupMap.put("7", GRAY);
+		legacyLookupMap.put("8", DARK_GRAY);
+		legacyLookupMap.put("0", BLACK);
+		
+		legacyLookupMap.put("k", "<obfuscated>");
+		legacyLookupMap.put("l", "<bold>");
+		legacyLookupMap.put("m", "<strikethrough>");
+		legacyLookupMap.put("n", "<underline>");
+		legacyLookupMap.put("o", "<italic>");
+		legacyLookupMap.put("r", "<reset>");
+	}
 
 	public static String strip(String line) {
 		return ChatColor.stripColor(line);
@@ -39,53 +91,47 @@ public class Colors {
 		return Component.text(TownyComponents.plain(component));
 	}
 
-	public static String translateColorCodes(String str) {
-		String out = ChatColor.translateAlternateColorCodes('&', str);
+	public static String translateColorCodes(String input) {
+		String out = translateLegacyCharacters(input);
+
 		return StringMgmt.translateHexColors(out);
+	}
+	
+	private static String translateLegacyCharacters(String input) {
+		final Matcher matcher = legacyPattern.matcher(input);
+
+		while (matcher.find()) {
+			String legacy = matcher.group();
+			input = input.replace(legacy, legacyLookupMap.getOrDefault(legacy.substring(1), legacy));
+		}
+
+		return input;
 	}
 
 	/**
-	 * @param colorCode The color code.
+	 * @param legacyColorCode The color code.
 	 * @return the {@link NamedTextColor} for the entered color string, or null if it is invalid.
 	 */
-	@Nullable
-	public static NamedTextColor toNamedTextColor(String colorCode) {
-		switch(colorCode) {
-			case "\u00A70":
-				return NamedTextColor.BLACK;
-			case "\u00A71":
-				return NamedTextColor.DARK_BLUE;
-			case "\u00A72":
-				return NamedTextColor.DARK_GREEN;
-			case "\u00A73":
-				return NamedTextColor.DARK_AQUA;
-			case "\u00A74":
-				return NamedTextColor.DARK_RED;
-			case "\u00A75":
-				return NamedTextColor.DARK_PURPLE;
-			case "\u00A76":
-				return NamedTextColor.GOLD;
-			case "\u00A77":
-				return NamedTextColor.GRAY;
-			case "\u00A78":
-				return NamedTextColor.DARK_GRAY;
-			case "\u00A79":
-				return NamedTextColor.BLUE;
-			case "\u00A7a":
-				return NamedTextColor.GREEN;
-			case "\u00A7b":
-				return NamedTextColor.AQUA;
-			case "\u00A7c":
-				return NamedTextColor.RED;
-			case "\u00A7d":
-				return NamedTextColor.LIGHT_PURPLE;
-			case "\u00A7e":
-				return NamedTextColor.YELLOW;
-			case "\u00A7f":
-				return NamedTextColor.WHITE;
-			default:
-				return null;
-		}
+	public static @Nullable NamedTextColor toNamedTextColor(String legacyColorCode) {
+		return switch (legacyColorCode) {
+			case "\u00A70" -> NamedTextColor.BLACK;
+			case "\u00A71" -> NamedTextColor.DARK_BLUE;
+			case "\u00A72" -> NamedTextColor.DARK_GREEN;
+			case "\u00A73" -> NamedTextColor.DARK_AQUA;
+			case "\u00A74" -> NamedTextColor.DARK_RED;
+			case "\u00A75" -> NamedTextColor.DARK_PURPLE;
+			case "\u00A76" -> NamedTextColor.GOLD;
+			case "\u00A77" -> NamedTextColor.GRAY;
+			case "\u00A78" -> NamedTextColor.DARK_GRAY;
+			case "\u00A79" -> NamedTextColor.BLUE;
+			case "\u00A7a" -> NamedTextColor.GREEN;
+			case "\u00A7b" -> NamedTextColor.AQUA;
+			case "\u00A7c" -> NamedTextColor.RED;
+			case "\u00A7d" -> NamedTextColor.LIGHT_PURPLE;
+			case "\u00A7e" -> NamedTextColor.YELLOW;
+			case "\u00A7f" -> NamedTextColor.WHITE;
+			default -> null;
+		};
 	}
 
 	public static String colorTown(String townName) {
