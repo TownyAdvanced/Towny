718da4fe632746460326aaf52ce2f2361e146747
Llm Dl <LlmDlio@gmail.com>
2022-05-07 16:47:30 -0500

    - Skip drawing SpawnPoint particles in unloaded chunks.     - Closes #5899.

--- src/com/palmergames/bukkit/towny/object/SpawnPoint.java
+++ src/com/palmergames/bukkit/towny/object/SpawnPoint.java
@@ -39,13 +39,17 @@ public class SpawnPoint {
 	}
 
 	public void drawParticle() {
+		if (!location.isWorldLoaded() ||
+			!location.getWorld().getChunkAt(location).isLoaded())
+			return;
+
 		Location origin = centreLocation(location);
 		int i = 0;
 
 		for (RingCoord ringPosition : RING_PATTERN) {
-		    Location point = origin.clone().add(ringPosition.getX(), 0.0d, ringPosition.getZ());
-		    Bukkit.getScheduler().scheduleSyncDelayedTask(Towny.getPlugin(), ()-> Bukkit.getWorld(location.getWorld().getName()).spawnParticle(Particle.CRIT_MAGIC, point, 1, 0.0, 0.0, 0.0, 0.0), i*4);		    
-		    i++;
+			Location point = origin.clone().add(ringPosition.getX(), 0.0d, ringPosition.getZ());
+			Bukkit.getScheduler().scheduleSyncDelayedTask(Towny.getPlugin(), ()-> location.getWorld().spawnParticle(Particle.CRIT_MAGIC, point, 1, 0.0, 0.0, 0.0, 0.0), i*4);
+			i++;
 		}
 	}
 	
