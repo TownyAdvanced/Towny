60d70f594620625cc8f811b01cb3340e7740a469
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2022-05-07 23:37:30 +0200

    Check player name async on login

--- src/com/palmergames/bukkit/towny/listeners/TownyLoginListener.java
+++ src/com/palmergames/bukkit/towny/listeners/TownyLoginListener.java
@@ -12,47 +12,46 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
-
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
 
 public class TownyLoginListener implements Listener {
 	@EventHandler(priority = EventPriority.NORMAL)
-    public void onPlayerLogin(PlayerLoginEvent event) {
+    public void onPlayerLogin(AsyncPlayerPreLoginEvent event) {
 		String npcPrefix = TownySettings.getNPCPrefix();
 		String warChest = "towny-war-chest";
 	    String serverAccount = TownySettings.getString(ConfigNodes.ECO_CLOSED_ECONOMY_SERVER_ACCOUNT);		
 		boolean disallowed = false;
-		Player player = event.getPlayer();
 		TownyUniverse townyUniverse = TownyUniverse.getInstance();
 		
-		if (player.getName().startsWith(npcPrefix)) {
-			Resident npcRes = townyUniverse.getResident(player.getName());
+		if (event.getName().startsWith(npcPrefix)) {
+			Resident npcRes = townyUniverse.getResident(event.getUniqueId());
 			if (npcRes != null && npcRes.isMayor()) {
-				event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Towny is preventing you from logging in using this account name.");
+				event.disallow(Result.KICK_OTHER, "Towny is preventing you from logging in using this account name.");
 				disallowed = true;
 			}
-		} else if (player.getName().equals(warChest) || player.getName().equals(warChest.replace("-", "_"))){
+		} else if (event.getName().equals(warChest) || event.getName().equals(warChest.replace("-", "_"))){
 			// Deny because this is the warChest account.
-			event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Towny is preventing you from logging in using this account name.");
+			event.disallow(Result.KICK_OTHER, "Towny is preventing you from logging in using this account name.");
 			disallowed = true;
-		} else if (player.getName().equals(serverAccount) || player.getName().equals(serverAccount.replace("-", "_"))){
+		} else if (event.getName().equals(serverAccount) || event.getName().equals(serverAccount.replace("-", "_"))){
 			// Deny because this is the closed economy server account.
-			event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Towny is preventing you from logging in using this account name.");
+			event.disallow(Result.KICK_OTHER, "Towny is preventing you from logging in using this account name.");
 			disallowed = true;
-		} else if (player.getName().startsWith(TownySettings.getTownAccountPrefix()) || 
-				   player.getName().startsWith(TownySettings.getTownAccountPrefix().replace("-","_")) ||
-				   player.getName().startsWith(TownySettings.getNationAccountPrefix()) || 
-				   player.getName().startsWith(TownySettings.getNationAccountPrefix().replace("-","_")) ) {
-			event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Towny is preventing you from logging in using this account name.");
+		} else if (event.getName().startsWith(TownySettings.getTownAccountPrefix()) || 
+				   event.getName().startsWith(TownySettings.getTownAccountPrefix().replace("-","_")) ||
+				   event.getName().startsWith(TownySettings.getNationAccountPrefix()) || 
+				   event.getName().startsWith(TownySettings.getNationAccountPrefix().replace("-","_")) ) {
+			event.disallow(Result.KICK_OTHER, "Towny is preventing you from logging in using this account name.");
 			disallowed = true;
 		}
 		
 		if (disallowed) {
 			String ip = event.getAddress().toString().substring(1);
-			Towny.getPlugin().getLogger().warning("A player using the IP address " + ip + " tried to log in using an account name (" + event.getPlayer().getName() + ") which could damage your server's economy, but was prevented by Towny. Consider banning this IP address!");
+			Towny.getPlugin().getLogger().warning("A player using the IP address " + ip + " tried to log in using an account name (" + event.getName() + ") which could damage your server's economy, but was prevented by Towny. Consider banning this IP address!");
 			for (Player ops : Bukkit.getOnlinePlayers()) {     	 
         		if (ops.isOp() || ops.hasPermission("towny.admin"))
-        			TownyMessaging.sendMsg(ops, Translatable.of("msg_admin_blocked_login", ip, event.getPlayer().getName()));        		
+        			TownyMessaging.sendMsg(ops, Translatable.of("msg_admin_blocked_login", ip, event.getName()));        		
         	}
 		}
 	}
