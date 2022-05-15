4d3e22749e9931726b5a66067c76e22f5929a426
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2022-05-15 14:41:15 +0200

    API: Add AccountAuditor#getTransactions

--- src/com/palmergames/bukkit/towny/object/economy/AccountAuditor.java
+++ src/com/palmergames/bukkit/towny/object/economy/AccountAuditor.java
@@ -4,10 +4,18 @@ import java.util.List;
 
 public interface AccountAuditor extends AccountObserver {
 	/**
-	 * Gets the transactions associated with this account. This should
+	 * Gets the formatted transactions associated with this account. This should
 	 * return the history in chronological order.
 	 * 
-	 * @return The transaction history from this account.
+	 * @return The formatted transaction history from this account.
 	 */
 	List<String> getAuditHistory();
+
+	/**
+	 * Gets the transactions associated with this account. This should
+	 * return the history in chronological order.
+	 *
+	 * @return The transaction history from this account.
+	 */
+	List<BankTransaction> getTransactions();
 }
