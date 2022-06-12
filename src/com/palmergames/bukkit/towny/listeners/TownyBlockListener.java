abbafd737022d1968ca95dcf3acd6d1dc8fcd002
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2022-06-13 00:12:54 +0200

    Prevent sculk from spreading where it shouldn't

--- src/com/palmergames/bukkit/towny/listeners/TownyBlockListener.java
+++ src/com/palmergames/bukkit/towny/listeners/TownyBlockListener.java
@@ -37,6 +37,7 @@ import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.block.BlockPistonExtendEvent;
 import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.BlockSpreadEvent;
 import org.bukkit.event.block.EntityBlockFormEvent;
 import java.util.ArrayList;
 import java.util.List;
@@ -379,7 +380,7 @@ public class TownyBlockListener implements Listener {
 			event.setCancelled(true);
 	}
 
-	@EventHandler
+	@EventHandler(ignoreCancelled = true)
 	public void onBlockFertilize(BlockFertilizeEvent event) {
 		if (plugin.isError()) {
 			event.setCancelled(true);
@@ -392,5 +393,19 @@ public class TownyBlockListener implements Listener {
 		List<BlockState> allowed = BorderUtil.allowedBlocks(event.getBlocks(), event.getBlock(), event.getPlayer());
 		event.getBlocks().clear();
         event.getBlocks().addAll(allowed);
+	}
+	
+	@EventHandler(ignoreCancelled = true)
+	public void onBlockSpread(BlockSpreadEvent event) {
+		if (plugin.isError()) {
+			event.setCancelled(true);
+			return;
+		}
+
+		if (!TownyAPI.getInstance().isTownyWorld(event.getBlock().getWorld()))
+			return;
+		
+		if (event.getSource().getType().name().equals("SCULK_CATALYST"))
+			event.setCancelled(BorderUtil.allowedMove(event.getSource(), event.getBlock()));
 	}
 }
