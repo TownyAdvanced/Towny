c035fa60f9cddbed7630ca84fec07f6d511eb0f5
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2022-08-07 23:29:38 +0200

    Add Translator#component

--- src/com/palmergames/bukkit/towny/object/Translator.java
+++ src/com/palmergames/bukkit/towny/object/Translator.java
@@ -2,8 +2,12 @@ package com.palmergames.bukkit.towny.object;
 
 import java.util.Locale;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.CommandSender;
+import org.checkerframework.framework.qual.DefaultQualifier;
+import org.jetbrains.annotations.NotNull;
 
+@DefaultQualifier(NotNull.class)
 public class Translator {
 	private final Locale locale;
 	
@@ -25,5 +29,13 @@ public class Translator {
 	
 	public String of(String key, Object... args) {
 		return Translation.of(key, locale, args);
+	}
+	
+	public Component component(String key) {
+		return Translatable.of(key).locale(this.locale).component();
+	}
+	
+	public Component component(String key, Object... args) {
+		return Translatable.of(key).locale(this.locale).component();
 	}
 }
