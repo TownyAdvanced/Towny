cdefb2ed49ad28597a46c12f8577dd337e5ef06f
LlmDl <llmdlio@gmail.com>
2022-10-19 09:43:10 -0500

    Javadoc for CancellableTownyEvent.

--- src/com/palmergames/bukkit/towny/event/CancellableTownyEvent.java
+++ src/com/palmergames/bukkit/towny/event/CancellableTownyEvent.java
@@ -4,7 +4,27 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 
+import com.palmergames.bukkit.towny.confirmations.ConfirmationBuilder;
+import com.palmergames.bukkit.towny.exceptions.CancelledEventException;
+import com.palmergames.bukkit.util.BukkitTools;
+
+/**
+ * A class extended by Towny Events which are Cancellable and which contain a
+ * cancelMessage.
+ * 
+ * Having a getCancelMessage() available to us allows us to throw a
+ * {@link CancelledEventException} which can use the cancelMessage in feedback
+ * messaging or logging.
+ * 
+ * Used primarily in the
+ * {@link ConfirmationBuilder#setCancellableEvent(CancellableTownyEvent)} and
+ * {@link BukkitTools#ifCancelledThenThrow(CancellableTownyEvent)} code.
+ * 
+ * @author LlmDL
+ * @since 0.98.4.0.
+ */
 public abstract class CancellableTownyEvent extends Event implements Cancellable {
 	private static final HandlerList handlers = new HandlerList();
 	private boolean isCancelled = false;
@@ -14,6 +34,8 @@ public abstract class CancellableTownyEvent extends Event implements Cancellable
 		super(!Bukkit.getServer().isPrimaryThread());
 	}
 
+	@NotNull
+	@Override
 	public HandlerList getHandlers() {
 		return handlers;
 	}
@@ -23,7 +45,7 @@ public abstract class CancellableTownyEvent extends Event implements Cancellable
 	}
 
 	/**
-	 * Whether the event has been cancelled.
+	 * @return true if this event is cancelled.
 	 */
 	@Override
 	public boolean isCancelled() {
@@ -31,7 +53,9 @@ public abstract class CancellableTownyEvent extends Event implements Cancellable
 	}
 
 	/**
-	 * Set the event to cancelled.
+	 * Sets the event to be cancelled or not.
+	 * 
+	 * @param cancelled true will cancel the event.
 	 */
 	@Override
 	public void setCancelled(boolean cancelled) {
@@ -39,15 +63,19 @@ public abstract class CancellableTownyEvent extends Event implements Cancellable
 	}
 
 	/**
-	 * @return the String message which will display to the player/sender.
+	 * @return cancelMessage a String which will be displayed to the player/sender
+	 *         informing them that the action they commited is being denied, if
+	 *         cancelMessage is blank then no message will be displayed.
 	 */
 	public String getCancelMessage() {
 		return cancelMessage;
 	}
 
 	/**
-	 * Sets the cancellation message which will display to the player. Set to "" to display nothing.
-	 * @param msg Message to display.
+	 * Sets the cancellation message which will display to the player. Set to "" to
+	 * display nothing.
+	 * 
+	 * @param msg cancelMessage to display as feedback.
 	 */
 	public void setCancelMessage(String msg) {
 		this.cancelMessage = msg;
