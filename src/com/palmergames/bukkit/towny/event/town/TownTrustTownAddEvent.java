c44a89d94c21b10929d624328411bb3bc179a5de
Kali0033 <90512509+Kali0033@users.noreply.github.com>
2022-11-05 02:20:52 +1000

    Add HanderList implementation to match newest Towny

--- src/com/palmergames/bukkit/towny/event/town/TownTrustTownAddEvent.java
+++ src/com/palmergames/bukkit/towny/event/town/TownTrustTownAddEvent.java
@@ -4,12 +4,14 @@ import com.palmergames.bukkit.towny.event.CancellableTownyEvent;
 import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.Translation;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
 public class TownTrustTownAddEvent extends CancellableTownyEvent {
 	private final Town town;
 	private final Town trustTown;
 	private final CommandSender sender;
+	private static final HandlerList handlers = new HandlerList();
 
 	public TownTrustTownAddEvent(CommandSender sender, Town trustTown, Town town) {
 		this.town = town;
@@ -18,6 +20,16 @@ public class TownTrustTownAddEvent extends CancellableTownyEvent {
 		setCancelMessage(Translation.of("msg_err_command_disable"));
 	}
 
+	@NotNull
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+	
 	/**
 	 * @return The town where the town is being added as trusted.
 	 */
