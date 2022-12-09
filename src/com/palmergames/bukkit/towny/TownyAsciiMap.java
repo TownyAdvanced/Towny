812a0d55b681c40f18fa8b91ecffe046c682f8c3
LlmDl <llmdlio@gmail.com>
2022-12-09 09:25:43 -0600

    - Allow the TownyMap system to properly parse Unicode characters U10000 and greater.     - Anyone using unicode characters for the TownBlockType map symbols should already be using the \u#### format in the config.     - The value you use should be the HTML-code variable of the Unicode you want to use.     - You can now begin using \u10000 and larger.

--- src/com/palmergames/bukkit/towny/TownyAsciiMap.java
+++ src/com/palmergames/bukkit/towny/TownyAsciiMap.java
@@ -10,7 +10,6 @@ import java.util.Map;
 
 import com.palmergames.bukkit.towny.utils.TownyComponents;
 
-import org.apache.commons.text.StringEscapeUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
@@ -291,14 +290,16 @@ public class TownyAsciiMap {
 	}
 
 	public static String parseSymbol(String symbol) {
-		if (symbol.startsWith("\\"))
-			return symbol.length() > 6 ? parseSupplementaryUnicode(symbol) : StringEscapeUtils.unescapeJava(symbol);
+		if (symbol.startsWith("\\u"))
+			return parseUnicode(symbol);
+//			return symbol.length() > 6 ? parseSupplementaryUnicode(symbol) : StringEscapeUtils.unescapeJava(symbol);
 		else 
 			return symbol.substring(0, 1);
 
 	}
 
-	private static String parseSupplementaryUnicode(String symbol) {
+	private static String parseUnicode(String symbol) {
+		// remove the "\\u" before we get the resulting unicode symbol.
 		return String.valueOf(Character.toChars(Integer.parseInt(symbol.substring(2))));
 	}
 }
