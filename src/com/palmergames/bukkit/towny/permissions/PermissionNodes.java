bfa556421c1223d879d191b295364da9c471efe1
LlmDl <llmdlio@gmail.com>
2022-12-21 15:26:27 -0600

    - New Permission Nodes:     - towny.town.spawn.nation.bypasspublic:       - description: User is permitted to use /town spawn when:         - they are a member of the same nation and,         - the town isn't public and,         - is_nation_ally_spawning_requiring_public_status is true.       - default: false, and not a child node of towny.town.spawn.*     - towny.town.spawn.ally.bypasspublic:       - description: User is permitted to use /town spawn when:         - they are a member of an allied nation and,         - the town isn't public and,         - is_nation_ally_spawning_requiring_public_status is true.       - default: false, and not a child node of towny.town.spawn.*     - Give either of these nodes to allow a king or otherwise special nation member to still travel       to an allied town when other nation members/allies are stopped, because the town is not public,       and the config requires public status.     - Closes #6367.

--- src/com/palmergames/bukkit/towny/permissions/PermissionNodes.java
+++ src/com/palmergames/bukkit/towny/permissions/PermissionNodes.java
@@ -433,7 +433,9 @@ public enum PermissionNodes {
 	TOWNY_SPAWN_TOWN("towny.town.spawn.town"),
 	TOWNY_SPAWN_OUTPOST("towny.town.spawn.outpost"),
 	TOWNY_SPAWN_NATION("towny.town.spawn.nation"),
+	TOWNY_SPAWN_NATION_BYPASS_PUBLIC("towny.town.spawn.nation.bypass_public"),
 	TOWNY_SPAWN_ALLY("towny.town.spawn.ally"),
+	TOWNY_SPAWN_ALLY_BYPASS_PUBLIC("towny.town.spawn.ally.bypass_public"),
 	TOWNY_SPAWN_PUBLIC("towny.town.spawn.public"),
 	
 	TOWNY_NATION_SPAWN_ALL("towny.nation.spawn.*"),
