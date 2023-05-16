ca696b05804b362d1b8d8e862407102b584b0675
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2023-05-16 21:04:15 +0200

    Some entity type renames

--- Towny/src/main/java/com/palmergames/bukkit/config/ConfigNodes.java
+++ Towny/src/main/java/com/palmergames/bukkit/config/ConfigNodes.java
@@ -588,7 +588,7 @@ public enum ConfigNodes {
 			"# You can adjust this setting for an existing world using /townyworld toggle revertentityexpl"),
 	NWS_PLOT_MANAGEMENT_WILD_ENTITY_REVERT_LIST(
 			"new_world_settings.plot_management.wild_revert_on_mob_explosion.entities",			
-			"CREEPER,ENDER_CRYSTAL,ENDER_DRAGON,FIREBALL,SMALL_FIREBALL,FIREBALL,PRIMED_TNT,MINECART_TNT,WITHER,WITHER_SKULL",
+			"CREEPER,ENDER_CRYSTAL,ENDER_DRAGON,FIREBALL,SMALL_FIREBALL,TNT,TNT_MINECART,WITHER,WITHER_SKULL",
 			"",
 			"# The list of entities whose explosions should be reverted."),
 	NWS_PLOT_MANAGEMENT_WILD_MOB_REVERT_TIME(
