ea6226722bf9f1fafac2fe3303a819e19f0e2173
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2023-06-01 01:15:50 +0200

    Use a more suitable comparator for our caches

--- Towny/src/main/java/com/palmergames/bukkit/towny/object/comparators/NationComparators.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/object/comparators/NationComparators.java
@@ -8,5 +8,5 @@ import java.util.Comparator;
  * A list of static comparators used for organizing lists of {@link Nation}'s
  */
 public class NationComparators {
-	public static final Comparator<Nation> BY_NUM_TOWNS = (n1, n2) -> n2.getTowns().size() - n1.getTowns().size();
+	public static final Comparator<Nation> BY_NUM_TOWNS = Comparator.comparingInt(nation -> nation.getTowns().size());
 }
