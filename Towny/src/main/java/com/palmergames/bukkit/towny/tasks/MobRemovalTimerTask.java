19287c811075c7f9e2b9152ab575bb98571be241
Agaloth <33335971+Agaloth@users.noreply.github.com>
2023-06-07 05:31:00 +0200

    Update MobRemovalTimerTask.java

--- Towny/src/main/java/com/palmergames/bukkit/towny/tasks/MobRemovalTimerTask.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/tasks/MobRemovalTimerTask.java
@@ -32,7 +32,7 @@ public class MobRemovalTimerTask extends TownyTimerTask {
 		super(plugin);
 
 		classesOfWorldMobsToRemove = EntityTypeUtil.parseLivingEntityClassNames(TownySettings.getWorldMobRemovalEntities(), "WorldMob: ");
-		classesOfWildernessMobsToRemove = EntityTypeUtil.parseLivingEntityClassNames(TownySettings.getWildernessMobRemovalEntities(),"WildernessMob: ");
+		classesOfWildernessMobsToRemove = EntityTypeUtil.parseLivingEntityClassNames(TownySettings.getWildernessMobRemovalEntities(), "WildernessMob: ");
 		classesOfTownMobsToRemove = EntityTypeUtil.parseLivingEntityClassNames(TownySettings.getTownMobRemovalEntities(), "TownMob: ");
 		isRemovingKillerBunny = TownySettings.isRemovingKillerBunny();
 	}
@@ -40,9 +40,9 @@ public class MobRemovalTimerTask extends TownyTimerTask {
 	public static boolean isRemovingWorldEntity(LivingEntity livingEntity) {
 		return EntityTypeUtil.isInstanceOfAny(classesOfWorldMobsToRemove, livingEntity);
 	}
-	
+
 	public static boolean isRemovingWildernessEntity(LivingEntity livingEntity) {
-		return  EntityTypeUtil.isInstanceOfAny(classesOfWildernessMobsToRemove, livingEntity);
+		return EntityTypeUtil.isInstanceOfAny(classesOfWildernessMobsToRemove, livingEntity);
 	}
 
 	public static boolean isRemovingTownEntity(LivingEntity livingEntity) {
@@ -66,88 +66,81 @@ public class MobRemovalTimerTask extends TownyTimerTask {
 			final List<LivingEntity> entities = world.getLivingEntities();
 			if (entities.isEmpty())
 				continue;
-			
-			CompletableFuture.runAsync(() -> {
-				// Build a list of mobs to be removed
-				List<LivingEntity> entitiesToRemove = new ArrayList<>();
 
-				for (LivingEntity entity : entities) {
-					Location livingEntityLoc = entity.getLocation();
+			// Build a list of mobs to be removed
+			List<LivingEntity> entitiesToRemove = new ArrayList<>();
 
-					// Check if entity is a Citizens NPC
-					if (PluginIntegrations.getInstance().checkCitizens(entity))
-						continue;
+			for (LivingEntity entity : entities) {
+				Location livingEntityLoc = entity.getLocation();
 
-					// Handles entities Globally.
-					if (!townyWorld.hasWorldMobs() && isRemovingWorldEntity(entity)) {
-						entitiesToRemove.add(entity);
-						continue;
-					}
+				// Check if entity is a Citizens NPC
+				if (PluginIntegrations.getInstance().checkCitizens(entity))
+					continue;
 
-					TownBlock townBlock = TownyAPI.getInstance().getTownBlock(livingEntityLoc);
-					
-					// Handles entities in the wilderness.
-					if (townBlock == null) {
-						if (townyWorld.hasWildernessMobs() || !isRemovingWildernessEntity(entity))
-							continue;
-					} else {
-						// The entity is inside of a town.
-
-						// Check if mobs are always allowed inside towns in this world, if the townblock allows it, or if the town has mobs forced on.
-						if (townyWorld.isForceTownMobs() || townBlock.getPermissions().mobs || townBlock.getTownOrNull().isAdminEnabledMobs())
-							continue;
-
-						// Check that Towny is removing this type of entity inside towns.
-						if (!isRemovingTownEntity(entity))
-							continue;
-					}
-
-					// Check if this is an EliteMob before we do any skipping-removal-of-named-mobs.
-					if (PluginIntegrations.getInstance().checkHostileEliteMobs(entity)) {
-						entitiesToRemove.add(entity);
-						continue;
-					}
-
-					if (TownySettings.isSkippingRemovalOfNamedMobs() && entity.getCustomName() != null)
-						continue;
-
-					// Special check if it's a rabbit, for the Killer Bunny variant.
-					if (entity instanceof Rabbit rabbit) {
-						if (isRemovingKillerBunny && rabbit.getRabbitType() == Rabbit.Type.THE_KILLER_BUNNY) {
-							entitiesToRemove.add(entity);
-							continue;
-						}
-					}
-
-					// Ensure the entity hasn't been removed since
-					if (entity.isDead())
-						continue;
-
-					if (!skipRemovalEvent && BukkitTools.isEventCancelled(new MobRemovalEvent(entity)))
-						continue;
-
+				// Handles entities Globally.
+				if (!townyWorld.hasWorldMobs() && isRemovingWorldEntity(entity)) {
 					entitiesToRemove.add(entity);
+					continue;
 				}
-				
-				if (entitiesToRemove.isEmpty())
-					return;
-				
-				if (plugin.isFolia()) {
-					for (LivingEntity entity : entitiesToRemove) {
-						plugin.getScheduler().run(entity, entity::remove);
-					}
+
+				TownBlock townBlock = TownyAPI.getInstance().getTownBlock(livingEntityLoc);
+
+				// Handles entities in the wilderness.
+				if (townBlock == null) {
+					if (townyWorld.hasWildernessMobs() || !isRemovingWildernessEntity(entity))
+						continue;
 				} else {
-					// Remove all mobs in one go if we're on Bukkit
-					plugin.getScheduler().run(() -> {
-						for (LivingEntity entity : entitiesToRemove) {
-							entity.remove();
-						}
-					});
+					// The entity is inside of a town.
+
+					// Check if mobs are always allowed inside towns in this world, if the townblock allows it, or if the town has mobs forced on.
+					if (townyWorld.isForceTownMobs() || townBlock.getPermissions().mobs || townBlock.getTownOrNull().isAdminEnabledMobs())
+						continue;
+
+					// Check that Towny is removing this type of entity inside towns.
+					if (!isRemovingTownEntity(entity))
+						continue;
 				}
-			}).whenComplete((v, t) -> {
-				if (t != null)
-					plugin.getLogger().log(Level.WARNING, "An error occurred while removing mobs in " + world.getName() + ":", t);
-			});
+
+				// Check if this is an EliteMob before we do any skipping-removal-of-named-mobs.
+				if (PluginIntegrations.getInstance().checkHostileEliteMobs(entity)) {
+					entitiesToRemove.add(entity);
+					continue;
+				}
+
+				if (TownySettings.isSkippingRemovalOfNamedMobs() && entity.getCustomName() != null)
+					continue;
+
+				// Special check if it's a rabbit, for the Killer Bunny variant.
+				if (entity instanceof Rabbit rabbit) {
+					if (isRemovingKillerBunny && rabbit.getRabbitType() == Rabbit.Type.THE_KILLER_BUNNY) {
+						entitiesToRemove.add(entity);
+						continue;
+					}
+				}
+
+				// Ensure the entity hasn't been removed since
+				if (entity.isDead())
+					continue;
+
+				if (!skipRemovalEvent && BukkitTools.isEventCancelled(new MobRemovalEvent(entity)))
+					continue;
+
+				entitiesToRemove.add(entity);
+			}
+
+			if (entitiesToRemove.isEmpty())
+				return;
+
+			if (plugin.isFolia()) {
+				for (LivingEntity entity : entitiesToRemove)
+					plugin.getScheduler().run(entity, entity::remove);
+			} else {
+				// Remove all mobs in one go if we're on Bukkit
+				plugin.getScheduler().run(() -> {
+					for (LivingEntity entity : entitiesToRemove)
+						entity.remove();
+				});
+			}
 		}
 	}
 }
