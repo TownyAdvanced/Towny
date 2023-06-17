1dc81b7dde94343924d56b6a85b3bf8cb556ecab
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2023-06-17 21:18:21 +0200

    method

--- Towny/src/main/java/com/palmergames/bukkit/config/migration/WorldMigrationAction.java
+++ Towny/src/main/java/com/palmergames/bukkit/config/migration/WorldMigrationAction.java
@@ -1,6 +1,8 @@
 package com.palmergames.bukkit.config.migration;
 
 import com.palmergames.bukkit.towny.object.TownyWorld;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,7 +15,7 @@ public enum WorldMigrationAction {
 	UPDATE_WORLD_BLOCK_IGNORE(((townyWorld, change) -> {
 		if (change.type == MigrationType.REPLACE)
 			townyWorld.setPlotManagementIgnoreIds(replaceAll(townyWorld.getPlotManagementIgnoreIds().stream()
-					.map(mat -> mat.getKey().getKey())
+					.map(WorldMigrationAction::matName)
 					.collect(Collectors.toList()), change.key, change.value));
 		else
 			townyWorld.setPlotManagementIgnoreIds(splitMats(townyWorld.getPlotManagementIgnoreIds().stream().map(type -> type.getKey().toString()).collect(Collectors.joining(",")) + change.value));
@@ -21,7 +23,7 @@ public enum WorldMigrationAction {
 	UPDATE_WORLD_DELETE_MAYOR(((townyWorld, change) -> {
 		if (change.type == MigrationType.REPLACE)
 			townyWorld.setPlotManagementMayorDelete(replaceAll(townyWorld.getPlotManagementMayorDelete().stream()
-					.map(mat -> mat.getKey().getKey())
+					.map(WorldMigrationAction::matName)
 					.collect(Collectors.toList()), change.key, change.value));
 		else
 			townyWorld.setPlotManagementMayorDelete(splitMats(townyWorld.getPlotManagementMayorDelete().stream().map(type -> type.getKey().toString()).collect(Collectors.joining(",")) + change.value));
@@ -29,7 +31,7 @@ public enum WorldMigrationAction {
 	UPDATE_WORLD_UNCLAIM_DELETE(((townyWorld, change) -> {
 		if (change.type == MigrationType.REPLACE)
 			townyWorld.setPlotManagementDeleteIds(replaceAll(townyWorld.getPlotManagementDeleteIds().stream()
-					.map(mat -> mat.getKey().getKey())
+					.map(WorldMigrationAction::matName)
 					.collect(Collectors.toList()), change.key, change.value));
 		else
 			townyWorld.setPlotManagementDeleteIds(splitMats(townyWorld.getPlotManagementDeleteIds().stream().map(type -> type.getKey().toString()).collect(Collectors.joining(",")) + change.value));
@@ -37,7 +39,7 @@ public enum WorldMigrationAction {
 	UPDATE_WILDERNESS_IGNORE_MATS(((townyWorld, change) -> {
 		if (change.type == MigrationType.REPLACE)
 			townyWorld.setUnclaimedZoneIgnore(replaceAll(townyWorld.getUnclaimedZoneIgnoreMaterials().stream()
-					.map(mat -> mat.getKey().getKey())
+					.map(WorldMigrationAction::matName)
 					.collect(Collectors.toList()), change.key, change.value));
 		else
 			townyWorld.setUnclaimedZoneIgnore(splitMats(townyWorld.getUnclaimedZoneIgnoreMaterials().stream().map(type -> type.getKey().toString()).collect(Collectors.joining(",")) + change.value));
@@ -45,7 +47,7 @@ public enum WorldMigrationAction {
 	UPDATE_WORLD_EXPLOSION_REVERT_BLOCKS(((townyWorld, change) -> {
 		if (change.type == MigrationType.REPLACE)
 			townyWorld.setPlotManagementWildRevertMaterials(replaceAll(townyWorld.getPlotManagementWildRevertBlocks().stream()
-					.map(mat -> mat.getKey().getKey())
+					.map(WorldMigrationAction::matName)
 					.collect(Collectors.toList()), change.key, change.value));
 		else
 			townyWorld.setPlotManagementWildRevertMaterials(splitMats(townyWorld.getPlotManagementWildRevertBlocks().stream().map(type -> type.getKey().toString()).collect(Collectors.joining(",")) + change.value));
@@ -53,7 +55,7 @@ public enum WorldMigrationAction {
 	UPDATE_WORLD_EXPLOSION_REVERT_ENTITIES(((townyWorld, change) -> {
 		if (change.type == MigrationType.REPLACE)
 			townyWorld.setPlotManagementWildRevertEntities(replaceAll(townyWorld.getPlotManagementWildRevertEntities().stream()
-					.map(type -> type.getKey().getKey())
+					.map(WorldMigrationAction::entityName)
 					.collect(Collectors.toList()), change.key, change.value));
 		else
 			townyWorld.setPlotManagementWildRevertEntities(splitMats(townyWorld.getPlotManagementWildRevertEntities().stream().map(type -> type.getKey().toString()).collect(Collectors.joining(",")) + change.value));
@@ -61,7 +63,7 @@ public enum WorldMigrationAction {
 	UPDATE_WORLD_UNCLAIM_REVERT_ENTITIES((((townyWorld, change) -> {
 		if (change.type == MigrationType.REPLACE)
 			townyWorld.setUnclaimDeleteEntityTypes(replaceAll(townyWorld.getUnclaimDeleteEntityTypes().stream()
-				.map(type -> type.getKey().getKey())
+				.map(WorldMigrationAction::entityName)
 				.collect(Collectors.toList()), change.key, change.value));
 		else
 			townyWorld.setUnclaimDeleteEntityTypes(splitMats(townyWorld.getUnclaimDeleteEntityTypes().stream().map(type -> type.getKey().toString()).collect(Collectors.joining(",")) + change.value));
@@ -91,5 +93,13 @@ public enum WorldMigrationAction {
 		}
 
 		return toReplace;
+	}
+	
+	private static String matName(Material material) {
+		return material.getKey().getKey(); // Material.DIRT -> minecraft:dirt -> dirt
+	}
+	
+	private static String entityName(EntityType entity) {
+		return entity.getKey().getKey();
 	}
 }
