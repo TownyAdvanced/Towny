edbeeead71fc871c635d911abe069141096bb829
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2023-07-02 23:21:33 +0200

    unused import

--- Towny/src/main/java/com/palmergames/bukkit/towny/command/TownyAdminCommand.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/command/TownyAdminCommand.java
@@ -80,7 +80,6 @@ import org.bukkit.permissions.Permission;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
