d123b52e7d2c4a5ba7a86e7fc0fea8333eaf86e4
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2023-10-23 23:17:37 +0200

    Make status screen sending respect the async eco setting (#7038)

--- Towny/src/main/java/com/palmergames/bukkit/towny/command/NationCommand.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/command/NationCommand.java
@@ -2588,7 +2588,7 @@ public class NationCommand extends BaseCommand implements CommandExecutor {
 		/*
 		 * This is run async because it will ping the economy plugin for the nation bank value.
 		 */
-		plugin.getScheduler().runAsync(() -> TownyMessaging.sendStatusScreen(sender, TownyFormatter.getStatus(nation, sender)));
+		TownyEconomyHandler.economyExecutor().execute(() -> TownyMessaging.sendStatusScreen(sender, TownyFormatter.getStatus(nation, sender)));
 	}
 	
 	/**
