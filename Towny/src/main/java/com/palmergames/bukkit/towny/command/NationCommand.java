ce3a2b01610679c2c83d6dae7a8157f15d5ba488
LlmDl <llmdlio@gmail.com>
2024-01-18 16:18:31 -0600

    Clean up nation join and add into a more complete system where all requirements are tested for.

--- Towny/src/main/java/com/palmergames/bukkit/towny/command/NationCommand.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/command/NationCommand.java
@@ -664,57 +664,6 @@ public class NationCommand extends BaseCommand implements CommandExecutor {
 		}
 	}
 
-	private void parseNationJoin(Player player, String[] args) {
-		
-		try {
-			Resident resident;
-			Town town;
-			Nation nation;
-			String nationName;
-
-			if (args.length < 1)
-				throw new TownyException(Translatable.of("msg_usage", "/nation join [nation]"));
-
-			nationName = args[0];
-			
-			resident = getResidentOrThrow(player);
-			town = resident.getTown();
-			nation = getNationOrThrow(nationName);
-
-			// Check if town is currently in a nation.
-			if (town.hasNation())
-				throw new TownyException(Translatable.of("msg_err_already_in_a_nation"));
-
-			// Check if town is town is free to join.
-			if (!nation.isOpen())
-				throw new TownyException(Translatable.of("msg_err_nation_not_open", nation.getFormattedName()));
-
-			// Check if the town is sanctioned and not allowed to join.
-			if (nation.hasSanctionedTown(town))
-				throw new TownyException(Translatable.of("msg_err_cannot_join_nation_sanctioned_town", nation.getName()));
-
-			if (!town.hasEnoughResidentsToJoinANation())
-				throw new TownyException(Translatable.of("msg_err_not_enough_residents_join_nation", town.getName()));
-
-			if (nation.hasReachedMaxTowns())
-				throw new TownyException(Translatable.of("msg_err_nation_over_town_limit", TownySettings.getMaxTownsPerNation()));
-
-			if (!nation.canAddResidents(town.getNumResidents()))
-				throw new TownyException(Translatable.of("msg_err_cannot_join_nation_over_resident_limit", TownySettings.getMaxResidentsPerNation()));
-
-			if (TownySettings.getNationProximityToCapital() > 0)
-				ProximityUtil.testTownProximityToNation(town, nation); // Throws TownyException with error message when it fails.
-
-			// Check if the command is not cancelled
-			BukkitTools.ifCancelledThenThrow(new NationPreAddTownEvent(nation, town));
-
-			nationAdd(nation, town);
-
-		} catch (TownyException e) {
-			TownyMessaging.sendErrorMsg(player, e.getMessage(player));
-		}
-	}
-
 	private void parseInviteCommand(Player player, String[] newSplit) throws TownyException {
 		Nation nation = getNationFromPlayerOrThrow(player);
 		String sent = Translatable.of("nation_sent_invites").forLocale(player)
@@ -1157,15 +1106,58 @@ public class NationCommand extends BaseCommand implements CommandExecutor {
 			HelpMenu.KING_HELP.send(player);
 	}
 
+
+	private void parseNationJoin(Player player, String[] args) throws TownyException {
+		if (args.length < 1)
+			throw new TownyException(Translatable.of("msg_usage", "/nation join [nation]"));
+
+		Town town = getTownFromPlayerOrThrow(player);
+		Nation nation = getNationOrThrow(args[0]);
+
+		// Check if the nation open able to be joined without an invite.
+		if (!nation.isOpen())
+			throw new TownyException(Translatable.of("msg_err_nation_not_open", nation.getFormattedName()));
+
+		// Vet whether the town can join the nation.
+		testNationAddTownOrThrow(town, nation);
+
+		// Actually go through with adding the town.
+		nationAdd(nation, town);
+	}
+
+	private void testNationAddTownOrThrow(Town town, Nation nation) throws TownyException {
+		// Check if town is currently in a nation.
+		if (nation.hasTown(town) || town.hasNation())
+			throw new TownyException(Translatable.of("msg_err_already_in_town", town.getName(), town.getNationOrNull().getName()));
+
+		if (!town.hasEnoughResidentsToJoinANation())
+			throw new TownyException(Translatable.of("msg_err_not_enough_residents_join_nation", town.getName()));
+
+		// Check if the town is sanctioned and not allowed to join.
+		if (nation.hasSanctionedTown(town))
+			throw new TownyException(Translatable.of("msg_err_cannot_join_nation_sanctioned_town", nation.getName()));
+
+		if (nation.hasReachedMaxTowns())
+			throw new TownyException(Translatable.of("msg_err_nation_over_town_limit", TownySettings.getMaxTownsPerNation()));
+
+		if (!nation.canAddResidents(town.getNumResidents()))
+			throw new TownyException(Translatable.of("msg_err_cannot_join_nation_over_resident_limit", TownySettings.getMaxResidentsPerNation()));
+
+		if (TownySettings.getNationProximityToCapital() > 0)
+			ProximityUtil.testTownProximityToNation(town, nation); // Throws TownyException with error message when it fails.
+
+		// Check if the command is not cancelled
+		BukkitTools.ifCancelledThenThrow(new NationPreAddTownEvent(nation, town));
+	}
+
 	/**
-	 * First stage of adding towns to a nation.
+	 * First stage of adding towns to a nation. We go through the player-submitted
+	 * list of Names, vet them for invite or invite-revocation, then either send
+	 * them an invite or revoke their invite when a town name is preceded by "-".
 	 * 
-	 * Tests here are performed to make sure Nation is allowed to add the towns:
-	 * - make sure the nation hasn't already hit the max towns (if that is required in teh config.)
-	 * 
-	 * @param player - Player using the command.
-	 * @param names - Names that will be matched to towns.
-	 * @throws TownyException generic
+	 * @param player Player using the command.
+	 * @param names  Names that will be matched to towns.
+	 * @throws TownyException when no towns were able to be invited.
 	 */
 	public void nationAdd(Player player, String[] names) throws TownyException {
 
@@ -1173,117 +1165,57 @@ public class NationCommand extends BaseCommand implements CommandExecutor {
 			throw new TownyException("Eg: /nation add [names]");
 
 		Nation nation = getNationFromPlayerOrThrow(player);
-		
-		if (nation.hasReachedMaxTowns())
-			throw new TownyException(Translatable.of("msg_err_nation_over_town_limit", TownySettings.getMaxTownsPerNation()));
 
-		// The list of valid invites.
-		List<String> newtownlist = new ArrayList<>();
-		// List of invites to be removed.
-		List<String> removeinvites = new ArrayList<>();
-		for (String townname : new ArrayList<>(Arrays.asList(names))) {
-			if (townname.startsWith("-")) {
-				// Add them to removing, remove the "-"
-				removeinvites.add(townname.substring(1));
-				continue;
-			}
+		// Our list of names to scan through.
+		List<String> nameList = new ArrayList<>(Arrays.asList(names));
 
-			Town town = null;
-			try {
-				town = getTownOrThrow(townname);
-			} catch (NotRegisteredException e) {
-				// The Town doesn't actually exist or was mis-spelled.
-				removeinvites.add(townname);
-				TownyMessaging.sendErrorMsg(player, Translatable.of("msg_err_not_registered_1", townname));
-				continue;
-			}
+		// Revoke invites from towns who have already had invites sent.
+		nameList.stream()
+				.filter(name -> name.startsWith("-") || nation.hasTown(name))
+				.map(name -> name.startsWith("-") ? name.substring(1) : name)
+				.filter(name -> TownyAPI.getInstance().getTown(name) != null)
+				.map(name -> TownyAPI.getInstance().getTown(name))
+				.filter(town -> nation.getSentInvites().stream().anyMatch(invite -> town.equals(invite.getReceiver())))
+				.forEach(town -> nationRevokeInviteTown(player, nation, town));
 
-			if (nation.hasTown(town) || town.hasNation()) {
-				// Town is already part of the nation.
-				removeinvites.add(townname);
-				TownyMessaging.sendErrorMsg(player, Translatable.of("msg_err_already_in_town", townname, town.getNationOrNull().getName()));
-				continue;
-			}
+		// Gather a list of Towns able to receive an invite to the nation, sending back
+		// error messages for the towns that cannot, finally dumping them to a list for
+		// the feedback message.
+		List<String> invitedNames = nameList.stream()
+				.filter(name -> !name.startsWith("-"))
+				.filter(name -> TownyAPI.getInstance().getTown(name) != null)
+				.map(name -> TownyAPI.getInstance().getTown(name))
+				.filter(town -> {
+					try {
+						// test that the town can join the nation. 
+						testNationAddTownOrThrow(town, nation);
+						// Send the actual invite to the town being added to the nation.
+						nationInviteTown(player, nation, town);
+					} catch (TownyException e) {
+						TownyMessaging.sendErrorMsg(player, e.getMessage(player));
+						return false;
+					}
+					return true;
+				})
+				.map(town -> town.getName())
+				.collect(Collectors.toList());
 
-			if (nation.hasSanctionedTown(town)) {
-				// Town is sanctioned and cannot join.
-				removeinvites.add(townname);
-				TownyMessaging.sendErrorMsg(player, Translatable.of("msg_err_cannot_add_sanctioned_town", townname));
-				continue;
-			}
-
-			if (!nation.canAddResidents(town.getNumResidents())) {
-				// Town has too many residents to join the nation
-				removeinvites.add(townname);
-				TownyMessaging.sendErrorMsg(player, Translatable.of("msg_err_cannot_join_nation_over_resident_limit", TownySettings.getMaxResidentsPerNation(), townname));
-				continue;
-			}
-
-			// add them to adding.
-			newtownlist.add(townname); 
-		}
-		names = newtownlist.toArray(new String[0]);
-		String[] namestoremove = removeinvites.toArray(new String[0]);
-		if (namestoremove.length >= 1) {
-			nationRevokeInviteTown(player, nation, TownyAPI.getInstance().getTowns(namestoremove));
-		}
-
-		if (names.length >= 1) {
-			nationAdd(player, nation, TownyAPI.getInstance().getTowns(names));
-		}
+		// Send the feedback message.
+		if (invitedNames.isEmpty())
+			throw new TownyException(Translatable.of("msg_invalid_name"));
+		else
+			TownyMessaging.sendPrefixedNationMessage(nation, Translatable.of("msg_invited_join_nation", player.getName(), String.join(", ", invitedNames)));
 	}
 
-	/**
-	 * Second stage of adding towns to a nation.
-	 * 
-	 * Tests here are performed to make sure the Towns are allowed to join the Nation:
-	 * - make sure the town has no nation.
-	 * - make sure the town has enough residents to join a nation (if it is required in the config.)
-	 * - make sure the town is close enough to the nation capital (if it is required in the config.)
-	 * 
-	 * Lastly, invites are sent and if successful, the third stage is called by the invite handler.
-	 * 
-	 * @param player player sending the request
-	 * @param nation Nation sending the request
-	 * @param invited the Town(s) being invited to the Nation
-	 * @throws TownyException executed when the arraylist (invited) returns empty (no valid town was entered)
-	 */
-	public static void nationAdd(Player player, Nation nation, List<Town> invited) throws TownyException {
-
-		List<String> invitedNames = new ArrayList<>(invited.size());
-		for (Town town : invited) {
-			if (town.hasNation()) {
-				TownyMessaging.sendErrorMsg(player, Translatable.of("msg_err_already_nation"));
-				continue;
-			}	
-
-			if (!town.hasEnoughResidentsToJoinANation()) {
-				TownyMessaging.sendErrorMsg(player, Translatable.of("msg_err_not_enough_residents_join_nation", town.getName()));
-				continue;
+	private static void nationRevokeInviteTown(CommandSender sender, Nation nation, Town town) {
+		InviteHandler.getActiveInvitesFor(nation, town).forEach(invite -> {
+			try {
+				InviteHandler.declineInvite(invite, true);
+				TownyMessaging.sendMsg(sender, Translatable.of("nation_revoke_invite_successful"));
+			} catch (InvalidObjectException e) {
+				plugin.getLogger().log(Level.WARNING, "unknown exception occurred while revoking invite", e);
 			}
-
-			if (TownySettings.getNationProximityToCapital() > 0) {
-				try {
-					ProximityUtil.testTownProximityToNation(town, nation);
-				} catch (TownyException e) {
-					TownyMessaging.sendErrorMsg(player, e.getMessage(player));
-					continue;
-				}
-			}
-
-			// Check if the command is not cancelled
-			BukkitTools.ifCancelledThenThrow(new NationPreAddTownEvent(nation, town));
-
-			nationInviteTown(player, nation, town);
-			invitedNames.add(town.getName());
-		}
-
-		if (invitedNames.size() > 0) {
-			TownyMessaging.sendPrefixedNationMessage(nation, Translatable.of("msg_invited_join_nation", player.getName(), String.join(", ", invitedNames)));
-		} else {
-			// This is executed when the arraylist returns empty (no valid town was entered).
-			throw new TownyException(Translatable.of("msg_invalid_name"));
-		}
+		});
 	}
 
 	/**
@@ -1321,6 +1253,13 @@ public class NationCommand extends BaseCommand implements CommandExecutor {
 			return;
 		}
 
+		if (nation.hasSanctionedTown(town)) {
+			// Nation has sanctioned this town, since inviting them.
+			TownyMessaging.sendPrefixedNationMessage(nation, Translatable.of("msg_err_cannot_add_sanctioned_town", town.getName()));
+			TownyMessaging.sendPrefixedTownMessage(town, Translatable.of("msg_err_cannot_join_nation_sanctioned_town", nation.getName()));
+			return;
+		}
+
 		if (nation.hasReachedMaxTowns()) {
 			// Nation has hit the max-towns limit.
 			TownyMessaging.sendPrefixedNationMessage(nation, Translatable.of("msg_err_nation_over_town_limit", TownySettings.getMaxTownsPerNation()));
@@ -1335,6 +1274,16 @@ public class NationCommand extends BaseCommand implements CommandExecutor {
 			return;
 		}
 
+		if (TownySettings.getNationProximityToCapital() > 0) {
+			try {
+				ProximityUtil.testTownProximityToNation(town, nation);
+			} catch (TownyException e) {
+				TownyMessaging.sendPrefixedNationMessage(nation, e.getMessage());
+				TownyMessaging.sendPrefixedTownMessage(town, e.getMessage());
+				return;
+			}
+		}
+
 		try {
 			town.setNation(nation);
 		} catch (AlreadyRegisteredException ignored) {}
@@ -1345,25 +1294,6 @@ public class NationCommand extends BaseCommand implements CommandExecutor {
 		TownyAPI.getInstance().getOnlinePlayers(town).forEach(p-> plugin.resetCache(p));
 	}
 
-	private static void nationRevokeInviteTown(CommandSender sender, Nation nation, List<Town> towns) {
-
-		for (Town town : towns) {
-			if (InviteHandler.inviteIsActive(nation, town)) {
-				for (Invite invite : town.getReceivedInvites()) {
-					if (invite.getSender().equals(nation)) {
-						try {
-							InviteHandler.declineInvite(invite, true);
-							TownyMessaging.sendMsg(sender, Translatable.of("nation_revoke_invite_successful"));
-							break;
-						} catch (InvalidObjectException e) {
-							plugin.getLogger().log(Level.WARNING, "unknown exception occurred while revoking invite", e);
-						}
-					}
-				}
-			}
-		}
-	}
-	
 	private static void nationInviteTown(Player player, Nation nation, Town town) throws TownyException {
 
 		TownJoinNationInvite invite = new TownJoinNationInvite(player, town, nation);
