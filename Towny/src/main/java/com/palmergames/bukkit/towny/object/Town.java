d05ab1c899b8be7f5ccfa016a0384ff49d6d4697
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2024-03-16 11:09:02 +0100

    -1 -1 -1

--- Towny/src/main/java/com/palmergames/bukkit/towny/object/Town.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/object/Town.java
@@ -1096,7 +1096,10 @@ public class Town extends Government implements TownBlockOwner {
 	}
 
 	public void setPlotPrice(double plotPrice) {
-		this.plotPrice = Math.min(Math.max(plotPrice, -1), TownySettings.getMaxPlotPrice());
+		if (plotPrice < 0)
+			plotPrice = -1;
+		
+		this.plotPrice = Math.min(plotPrice, TownySettings.getMaxPlotPrice());
 	}
 
 	public double getPlotPrice() {
@@ -1114,7 +1117,10 @@ public class Town extends Government implements TownBlockOwner {
 	}
 
 	public void setCommercialPlotPrice(double commercialPlotPrice) {
-		this.commercialPlotPrice = Math.min(Math.max(commercialPlotPrice, -1), TownySettings.getMaxPlotPrice());
+		if (commercialPlotPrice < 0)
+			commercialPlotPrice = -1;
+		
+		this.commercialPlotPrice = Math.min(commercialPlotPrice, TownySettings.getMaxPlotPrice());
 	}
 
 	public double getCommercialPlotPrice() {
@@ -1123,7 +1129,10 @@ public class Town extends Government implements TownBlockOwner {
 	}
 
 	public void setEmbassyPlotPrice(double embassyPlotPrice) {
-		this.embassyPlotPrice = Math.min(Math.max(embassyPlotPrice, -1), TownySettings.getMaxPlotPrice());
+		if (embassyPlotPrice < 0)
+			embassyPlotPrice = -1;
+		
+		this.embassyPlotPrice = Math.min(embassyPlotPrice, TownySettings.getMaxPlotPrice());
 	}
 
 	public double getEmbassyPlotPrice() {
