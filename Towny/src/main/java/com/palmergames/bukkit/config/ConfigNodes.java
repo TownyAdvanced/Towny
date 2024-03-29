f28f0aa6ab417d85b1b738d9835984a6e2f0bffd
LlmDl <llmdlio@gmail.com>
2024-03-29 11:46:52 -0500

    Add a couple of newlines missing from the config.

--- Towny/src/main/java/com/palmergames/bukkit/config/ConfigNodes.java
+++ Towny/src/main/java/com/palmergames/bukkit/config/ConfigNodes.java
@@ -462,10 +462,12 @@ public enum ConfigNodes {
 	NWS_PLOT_MANAGEMENT_WILD_REVERT_BLOCK_WHITELIST(
 		"new_world_settings.plot_management.wild_revert_on_explosion_block_whitelist",
 		"",
+		"",
 		"# This section is applied to new worlds as default settings when new worlds are detected.",
 		"# The list of blocks to regenerate for block and entity explosions. (if empty all blocks will regenerate)"),
 	NWS_PLOT_MANAGEMENT_WILD_REVERT_BLOCKS_TO_NOT_OVERWRITE(
 			"new_world_settings.plot_management.wild_revert_explosions_blocks_to_not_replace",
+			"",
 			"",
 			"# This section is applied to new worlds as default settings when new worlds are detected.",
 			"# This is the list of blocks that should not be overwritten by wilderness explosion reverts. (if empty all ",
