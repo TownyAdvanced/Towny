d69ef18f4308e44b9bcb9ce73799c2be2d2e5bda
LlmDl <llmdlio@gmail.com>
2024-07-28 08:06:12 -0500

    Add separate lang string for the already running warning.

--- Towny/src/main/java/com/palmergames/bukkit/towny/command/TownyAdminCommand.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/command/TownyAdminCommand.java
@@ -901,8 +901,10 @@ public class TownyAdminCommand extends BaseCommand implements CommandExecutor {
 
 	private void parseAdminNewDay(CommandSender sender) throws TownyException {
 		checkPermOrThrow(sender, PermissionNodes.TOWNY_COMMAND_TOWNYADMIN_NEWDAY.getNode());
-		if (NewDayScheduler.isNewDayRunning())
+		if (NewDayScheduler.isNewDayScheduled())
 			throw new TownyException(Translatable.of("msg_newday_already_scheduled_soon"));
+		if (NewDayScheduler.isNewDayRunning())
+			throw new TownyException(Translatable.of("msg_newday_already_running"));
 
 		// Turn the daily timer scheduler on if it wasn't running already.
 		if (!NewDayScheduler.isNewDaySchedulerRunning())
