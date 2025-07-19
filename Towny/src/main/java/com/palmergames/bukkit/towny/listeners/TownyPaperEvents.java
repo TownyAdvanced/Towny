cc0a33c77ea753da57d22c8c98c297376b4711a2
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2025-07-19 23:31:02 +0200

    Replace MethodHandles with method calls where possible

--- Towny/src/main/java/com/palmergames/bukkit/towny/listeners/TownyPaperEvents.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/listeners/TownyPaperEvents.java
@@ -1,5 +1,7 @@
 package com.palmergames.bukkit.towny.listeners;
 
+import com.destroystokyo.paper.event.block.BeaconEffectEvent;
+import com.destroystokyo.paper.event.entity.EnderDragonFireballHitEvent;
 import com.palmergames.bukkit.towny.Towny;
 import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.TownyMessaging;
@@ -18,7 +20,6 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
-import org.bukkit.entity.AreaEffectCloud;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -28,7 +29,6 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockEvent;
 import org.bukkit.event.block.TNTPrimeEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityEvent;
@@ -45,9 +45,6 @@ import java.util.logging.Level;
 public class TownyPaperEvents implements Listener {
 	private final Towny plugin;
 	
-	private static final String SPIGOT_PRIME_EVENT = "org.bukkit.event.block.TNTPrimeEvent"; // Added somewhere during 1.19.4
-	private static final String PAPER_PRIME_EVENT = "com.destroystokyo.paper.event.block.TNTPrimeEvent";
-	
 	private static final String SIGN_OPEN_EVENT = "io.papermc.paper.event.player.PlayerOpenSignEvent";
 	private static final String SPIGOT_SIGN_OPEN_EVENT = "org.bukkit.event.player.PlayerSignOpenEvent";
 	private static final String USED_SIGN_OPEN_EVENT = JavaUtil.classExists(SIGN_OPEN_EVENT) ? SIGN_OPEN_EVENT : SPIGOT_SIGN_OPEN_EVENT;
@@ -61,13 +58,6 @@ public class TownyPaperEvents implements Listener {
 	public static final MethodHandle SIGN_OPEN_GET_CAUSE = JavaUtil.getMethodHandle(USED_SIGN_OPEN_EVENT, "getCause");
 	private static final MethodHandle SIGN_OPEN_GET_SIGN = JavaUtil.getMethodHandle(USED_SIGN_OPEN_EVENT, "getSign");
 
-	private static final MethodHandle GET_ORIGIN = JavaUtil.getMethodHandle(Entity.class, "getOrigin");
-	private static final MethodHandle GET_PRIMER_ENTITY = JavaUtil.getMethodHandle(PAPER_PRIME_EVENT, "getPrimerEntity");
-
-	public static final MethodHandle DRAGON_FIREBALL_GET_EFFECT_CLOUD = JavaUtil.getMethodHandle(DRAGON_FIREBALL_HIT_EVENT, "getAreaEffectCloud");
-
-	public static final MethodHandle BEACON_EFFECT_GET_PLAYER = JavaUtil.getMethodHandle(BEACON_EFFECT_EVENT, "getPlayer");
-	
 	public static final String ADD_TO_WORLD_EVENT = "com.destroystokyo.paper.event.entity.EntityAddToWorldEvent";
 	
 	public TownyPaperEvents(Towny plugin) {
@@ -75,31 +65,17 @@ public class TownyPaperEvents implements Listener {
 	}
 	
 	public void register() {
-		if (JavaUtil.classExists(SPIGOT_PRIME_EVENT))
-			registerEvent(SPIGOT_PRIME_EVENT, this::tntPrimeEvent, EventPriority.LOW, true);
-		else if (GET_PRIMER_ENTITY != null) {
-			registerEvent(PAPER_PRIME_EVENT, this::tntPrimeEvent, EventPriority.LOW, true);
-			TownyMessaging.sendDebugMsg("TNTPRimeEvent#getPrimerEntity method found, using TNTPrimeEvent listener.");
-		}
-		
-		if (GET_ORIGIN != null) {
-			registerEvent(EntityChangeBlockEvent.class, fallingBlockListener(), EventPriority.LOW, true);
-			TownyMessaging.sendDebugMsg("Entity#getOrigin found, using falling block listener.");
-		}
+		registerEvent(TNTPrimeEvent.class, tntPrimeEvent(), EventPriority.LOW, true);
+		registerEvent(EntityChangeBlockEvent.class, fallingBlockListener(), EventPriority.LOW, true);
 		
 		if (SIGN_OPEN_GET_CAUSE != null) {
 			registerEvent(JavaUtil.classExists(SIGN_OPEN_EVENT) ? SIGN_OPEN_EVENT : SPIGOT_SIGN_OPEN_EVENT, this::openSignListener, EventPriority.LOW, true);
 			TownyMessaging.sendDebugMsg("PlayerOpenSignEvent#getCause found, using PlayerOpenSignEvent listener.");
 		}
 		
-		if (DRAGON_FIREBALL_GET_EFFECT_CLOUD != null) {
-			registerEvent(DRAGON_FIREBALL_HIT_EVENT, this::dragonFireballHitEventListener, EventPriority.LOW, true);
-			TownyMessaging.sendDebugMsg("Using " + DRAGON_FIREBALL_GET_EFFECT_CLOUD + " listener.");
-		}
-
-		if (BEACON_EFFECT_GET_PLAYER != null) {
+		if (JavaUtil.classExists(BEACON_EFFECT_EVENT)) {
 			registerEvent(BEACON_EFFECT_EVENT, this::beaconEffectEventListener, EventPriority.LOW, true);
-			TownyMessaging.sendDebugMsg("Using " + BEACON_EFFECT_GET_PLAYER + " listener.");
+			TownyMessaging.sendDebugMsg("Using " + BEACON_EFFECT_EVENT + " listener.");
 		}
 		
 		registerEvent(PLAYER_ELYTRA_BOOST_EVENT, this::playerElytraBoostListener, EventPriority.LOW, true);
@@ -133,31 +109,21 @@ public class TownyPaperEvents implements Listener {
 		};
 	}
 	
-	// https://papermc.io/javadocs/paper/1.19/com/destroystokyo/paper/event/block/TNTPrimeEvent.html
-	private Consumer<Event> tntPrimeEvent() {
+	private Consumer<TNTPrimeEvent> tntPrimeEvent() {
 		return event -> {
-			Entity primerEntity = null;
-			
-			if (event.getClass().getName().equals(SPIGOT_PRIME_EVENT)) {
-				primerEntity = ((TNTPrimeEvent) event).getPrimingEntity();
-			} else if (GET_PRIMER_ENTITY != null) {
-				try {
-					primerEntity = (Entity) GET_PRIMER_ENTITY.invoke(event);
-				} catch (final Throwable e) {
-					return;
-				}
-			}
+			Entity primerEntity = event.getPrimingEntity();
 
 			if (primerEntity instanceof Projectile projectile) {
-				Cancellable cancellable = (Cancellable) event;
-				Block block = ((BlockEvent) event).getBlock();
+				Block block = event.getBlock();
+
 				if (projectile.getShooter() instanceof Player player) {
 					// A player shot a flaming arrow at the block, use a regular destroy test.
-					cancellable.setCancelled(!TownyActionEventExecutor.canDestroy(player, block));
+					event.setCancelled(!TownyActionEventExecutor.canDestroy(player, block));
 				} else if (projectile.getShooter() instanceof BlockProjectileSource bps) {
 					// A block (likely a dispenser) shot a flaming arrow, cancel it if plot owners aren't the same.
-					if (!BorderUtil.allowedMove(bps.getBlock(), block))
-						cancellable.setCancelled(true);
+					if (!BorderUtil.allowedMove(bps.getBlock(), block)) {
+						event.setCancelled(true);
+					}
 				}
 			}
 		};
@@ -165,16 +131,10 @@ public class TownyPaperEvents implements Listener {
 	
 	private Consumer<EntityChangeBlockEvent> fallingBlockListener() {
 		return event -> {
-			if (GET_ORIGIN == null || event.getEntityType() != EntityType.FALLING_BLOCK || !TownyAPI.getInstance().isTownyWorld(event.getEntity().getWorld()))
+			if (event.getEntityType() != EntityType.FALLING_BLOCK || !TownyAPI.getInstance().isTownyWorld(event.getEntity().getWorld()))
 				return;
 			
-			Location origin;
-			try {
-				origin = (Location) GET_ORIGIN.invokeExact(event.getEntity());
-			} catch (final Throwable e) {
-				plugin.getLogger().log(Level.WARNING, "An exception occurred while invoking Entity#getOrigin reflectively", e);
-				return;
-			}
+			Location origin = event.getEntity().getOrigin();
 			
 			if (origin == null)
 				return;
@@ -211,40 +171,13 @@ public class TownyPaperEvents implements Listener {
 		};
 	}
 
-	private Consumer<Event> dragonFireballHitEventListener() {
+	private Consumer<BeaconEffectEvent> beaconEffectEventListener() {
 		return event -> {
-			if (DRAGON_FIREBALL_GET_EFFECT_CLOUD == null)
+			if (!TownySettings.beaconsForTownMembersOnly())
 				return;
 
-			final AreaEffectCloud effectCloud;
-
-			try {
-				effectCloud = (AreaEffectCloud) DRAGON_FIREBALL_GET_EFFECT_CLOUD.invoke(event);
-			} catch (final Throwable thr) {
-				plugin.getLogger().log(Level.WARNING, "An exception occurred when invoking " + DRAGON_FIREBALL_HIT_EVENT + "#getAreaEffectCloud reflectively.", thr);
-				return;
-			}
-
-			if (TownyEntityListener.discardAreaEffectCloud(effectCloud))
-				((Cancellable) event).setCancelled(true);
-		};
-	}
-	
-	private Consumer<Event> beaconEffectEventListener() {
-		return event -> {
-			if (BEACON_EFFECT_GET_PLAYER == null || !TownySettings.beaconsForTownMembersOnly())
-				return;
-
-			final Player player;
-			final Block block;
-
-			try {
-				player = (Player) BEACON_EFFECT_GET_PLAYER.invoke(event);
-				block = ((BlockEvent) event).getBlock();
-			} catch (final Throwable thr) {
-				plugin.getLogger().log(Level.WARNING, "An exception occurred when invoking " + BEACON_EFFECT_EVENT + "#getBlock or #getPlayer reflectively.", thr);
-				return;
-			}
+			final Player player = event.getPlayer();
+			final Block block = event.getBlock();
 
 			Town blockTown = TownyAPI.getInstance().getTown(block.getLocation());
 			Town playerTown = TownyAPI.getInstance().getTown(player);
