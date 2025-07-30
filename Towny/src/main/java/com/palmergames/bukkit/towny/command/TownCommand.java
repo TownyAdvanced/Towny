a9ab8280386ebccf3484e316fe1a251c95eb1d43
Lemar98 <lemar1032@gmail.com>
2025-07-30 22:45:49 +0300

    permission validation for town enemylist was added

--- Towny/src/main/java/com/palmergames/bukkit/towny/command/TownCommand.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/command/TownCommand.java
@@ -740,8 +740,8 @@ public class TownCommand extends BaseCommand implements CommandExecutor {
 	}
 
 	private void townEnemyList(Player player, String[] split) throws TownyException {
+		checkPermOrThrow(player, PermissionNodes.TOWNY_COMMAND_TOWN_ENEMYLIST.getNode());
 		Town town = split.length == 1 ? getTownFromPlayerOrThrow(player) : getTownOrThrow(split[1]);
-
 		if (town.getEnemies().isEmpty())
 			TownyMessaging.sendErrorMsg(player, Translatable.of("msg_error_town_has_no_enemies")); 
 		else {
