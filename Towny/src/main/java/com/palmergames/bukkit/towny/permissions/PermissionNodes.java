15ca8659bacaef879a3c0b302ee88da5e0eced3a
Lemar98 <lemar1032@gmail.com>
2025-07-30 19:33:20 +0300

    permission validation for town allylist command was added

--- Towny/src/main/java/com/palmergames/bukkit/towny/permissions/PermissionNodes.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/permissions/PermissionNodes.java
@@ -193,6 +193,7 @@ public enum PermissionNodes {
 		TOWNY_COMMAND_TOWN_BANKHISTORY("towny.command.town.bankhistory"),
 		TOWNY_COMMAND_TOWN_RESLIST("towny.command.town.reslist"),
 		TOWNY_COMMAND_TOWN_OUTLAWLIST("towny.command.town.outlawlist"),
+		TOWNY_COMMAND_TOWN_ALLYLIST("towny.command.town.allylist"),
 	
 	/*
 	 * Plot command permissions
