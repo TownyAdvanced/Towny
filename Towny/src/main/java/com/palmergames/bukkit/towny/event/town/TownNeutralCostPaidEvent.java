21020f812ff1278343ac54c74deddd19593f3dde
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2025-08-24 17:32:00 +0200

    typo

--- Towny/src/main/java/com/palmergames/bukkit/towny/event/town/TownNeutralCostPaidEvent.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/event/town/TownNeutralCostPaidEvent.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Fired when town has paid neutrality costs.
+ * Fired when a town has paid neutrality costs.
  */
 public class TownNeutralCostPaidEvent extends Event {
 	private static final HandlerList HANDLER_LIST = new HandlerList();
