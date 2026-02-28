621edf7999f0a09a84ed0657c8e0e3722345336f
LlmDl <LlmDlio@gmail.com>
2026-02-28 09:50:08 -0600

    Add Scoreboard/HUD support for Folia, convert Scoreboards to Adventure. (#8115)
    * Add Scoreboard/HUD support for Folia, convert Scoreboards to Adventure.
    
    Adds a ServerHUD which is either a FoliaHUD or PaperHUD.
    
    HUDImplementers like MapHUD and PermHUD be used to make instances of
    either PaperHUDs or FoliaHUD. MapHUD and PermHUD both require a HUD
    which is then used by FoliaHUD and PaperHUD.
    
    The HUDManager class is used to track Towny's MapHUD and PermHUD.
    
    * Refactoring:
    
    De-duplicates methods in the Paper/Folia HUD.
    
    Change Set<Player> to Set<UUID> in Paper/FoliaHUD.
    
    Change PermHUD and MapHUD constructors to take in HUD instead of
    building them internally.
    
    * Fix up the colouring shown in the PermHUD to use the suffix colour, and
    to reset the bold tags in the key.
    
    * Restore White colouring to board suffixes.
    
    Add miniMessage methods to shorten line length.
    
    * Fix extra spacing in keys
    
    * Couple more touch-ups to the perm hud, removing doubled up spaces.
    
    * Missing green colour in the MapHUD

---
+++ Towny/src/main/java/com/palmergames/bukkit/towny/huds/providers/PaperHUD.java
@@ -0,0 +1,123 @@
+package com.palmergames.bukkit.towny.huds.providers;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.jetbrains.annotations.Nullable;
+
+import com.palmergames.bukkit.towny.huds.HUDImplementer;
+import com.palmergames.bukkit.util.BukkitTools;
+
+import io.papermc.paper.scoreboard.numbers.NumberFormat;
+import net.kyori.adventure.text.Component;
+
+public class PaperHUD implements ServerHUD {
+
+	final String displayName;
+	final String objectiveName;
+	final Consumer<Player> playerConsumer;
+	final BiConsumer<Player, Object> playerWithObjectConsumer;
+
+	Map<UUID, Scoreboard> boardMap = new HashMap<>();
+	Set<UUID> playerUUIDs = new HashSet<>();
+
+	public PaperHUD(HUDImplementer implementer) {
+		HUD hud = implementer.getHUD();
+		this.displayName = hud.displayName;
+		this.objectiveName = hud.objectiveName;
+		this.playerConsumer = hud.playerConsumer;
+		this.playerWithObjectConsumer = hud.playerWithObjectConsumer;
+	}
+
+	public Set<UUID> getPlayerUUIDs() {
+		return playerUUIDs;
+	}
+
+	@Override
+	public boolean toggleOff(Player player) {
+		Optional.ofNullable(Bukkit.getScoreboardManager()).ifPresent(manager -> player.setScoreboard(manager.getMainScoreboard()));
+		removePlayer(player);
+		return true; 
+	}
+
+	@Override
+	public boolean toggleOn(Player player) {
+		Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
+		Objective objective = BukkitTools.objective(board, objectiveName, displayName);
+		objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+		objective.setAutoUpdateDisplay(false);
+		try {
+			objective.numberFormat(NumberFormat.blank());
+		} catch (NoSuchMethodError | NoClassDefFoundError ignored) {}
+
+		board.registerNewTeam(objectiveName);
+		boardMap.put(player.getUniqueId(), board);
+
+		addPlayer(player);
+		updateHUD(player);
+		player.setScoreboard(board);
+		return true;
+	}
+
+	@Override
+	public boolean isActive(Player player) {
+		return player.getScoreboard().getObjective(objectiveName) != null;
+	}
+
+	@Override
+	public void setTitle(UUID uuuid, Component title) {
+		Objective objective = getObjective(uuuid);
+		if (objective == null)
+			return;
+		objective.displayName(title);
+	}
+
+	@Override
+	public void setLines(UUID uuuid, List<Component> lines) {
+		Objective objective = getObjective(uuuid);
+		if (objective == null)
+			return;
+
+		while (lines.size() < 15)
+			lines.add(Component.empty()); // Pad the List until is is the max height for a scoreboard.
+
+		Collections.reverse(lines);
+		for (int i = 14; i >= 0 ; i--) {
+			String teamName = String.valueOf(i);
+			objective.getScore(teamName).setScore(i);
+			objective.getScore(teamName).customName(lines.get(i));
+		}
+	}
+
+	@Override
+	public void updateHUD(Player player, Object object) {
+		playerWithObjectConsumer.accept(player, object);
+	}
+
+	@Override
+	public void updateHUD(Player player) {
+		playerConsumer.accept(player);
+	}
+
+	@Nullable
+	private Objective getObjective(UUID uuuid) {
+		Scoreboard board = boardMap.get(uuuid);
+		if (board == null)
+			return null;
+		Objective objective = board.getObjective(objectiveName);
+		return objective;
+	}
+}
