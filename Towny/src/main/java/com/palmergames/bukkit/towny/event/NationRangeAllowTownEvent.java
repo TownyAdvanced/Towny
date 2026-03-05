cfa8590012319a74c289f3f319b23cccf9f18ec7
Veyronity <veyronitymc@gmail.com>
2026-03-05 11:24:08 +0200

    Add javadoc

--- Towny/src/main/java/com/palmergames/bukkit/towny/event/NationRangeAllowTownEvent.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/event/NationRangeAllowTownEvent.java
@@ -5,12 +5,20 @@ import com.palmergames.bukkit.towny.object.Town;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * An event fired before a town is deemed too far to be in the specified nation. May be fired pre-canceled due to Proximity rules.
+ * Plugins may use #setCancelled(false) to bypass proximity rules and forcefully allow the town to join, or stay if it already is a member.
+ */
 public class NationRangeAllowTownEvent extends CancellableTownyEvent {
 	private static final HandlerList HANDLER_LIST = new HandlerList();
 	
 	private final Nation nation;
 	private final Town town;
 
+	/**
+	 * An event fired before a town is deemed too far to be in the specified nation. May be fired pre-canceled due to Proximity rules.
+	 * Plugins may use #setCancelled(false) to bypass proximity rules and forcefully allow the town to join, or stay if it already is a member.
+	 */
 	public NationRangeAllowTownEvent(Nation nation, Town town) {
 		this.town = town;
 		this.nation = nation;
