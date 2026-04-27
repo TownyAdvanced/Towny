d18cfb8ca8714e019715432d6982d56d9b61af72
HydrolienF <hydrolien.f@gmail.com>
2026-04-27 15:48:44 +0200

    false by default

--- Towny/src/main/java/com/palmergames/bukkit/config/ConfigNodes.java
+++ Towny/src/main/java/com/palmergames/bukkit/config/ConfigNodes.java
@@ -1221,7 +1221,7 @@ public enum ConfigNodes {
 			"# will be ignored for towns that are mutually allied. Setting to false will keep all towns separated the same."),
 	CLAIMING_MIN_DISTANCE_IGNORED_FOR_RUINED_TOWN(
 			"claiming.distance_rules.min_distances_ignored_for_ruined_towns",
-			"true",
+			"false",
 			"",
 			"# If true, the below settings: min_plot_distance_from_town_plot and min_distance_from_town_homeblock",
 			"# will be ignored for towns that are ruined. Setting to false will keep all towns separated the same."),
