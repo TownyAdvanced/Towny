881dbd8c53dc197287217f4900283034ee112bea
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-06-19 20:42:15 +0200

    Use the pre spawn event for additionally preventing spawns

--- Towny/src/main/java/com/palmergames/bukkit/towny/event/mobs/MobSpawnRemovalEvent.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/event/mobs/MobSpawnRemovalEvent.java
@@ -1,30 +1,78 @@
 package com.palmergames.bukkit.towny.event.mobs;
 
-import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.jetbrains.annotations.ApiStatus;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
+import java.util.Objects;
+
+/**
+ * Called when Towny wants to prevent a mob from spawning. If canceled, the mob will be allowed to spawn, but follow-up removal attempts can still happen. These would be handled using {@link com.palmergames.bukkit.towny.event.MobRemovalEvent}.
+ */
 public class MobSpawnRemovalEvent extends Event implements Cancellable {
-	private static final HandlerList handlers = new HandlerList();
+	private static final HandlerList HANDLER_LIST = new HandlerList();
 
 	private boolean cancelled = false;
-	private final Entity entity;
+	private final @Nullable Entity entity;
 
-	public MobSpawnRemovalEvent(Entity entity) {
-		super(!Bukkit.getServer().isPrimaryThread());
+	private final EntityType entityType;
+	private final Location location;
+	private final CreatureSpawnEvent.SpawnReason spawnReason;
+
+	@ApiStatus.Internal
+	public MobSpawnRemovalEvent(final @Nullable Entity entity, final EntityType entityType, final Location location, final CreatureSpawnEvent.SpawnReason spawnReason) {
 		this.entity = entity;
+		this.entityType = entityType;
+		this.location = location;
+		this.spawnReason = spawnReason;
 	}
 
-	@Override
-	public HandlerList getHandlers() {
-		return handlers;
+	/**
+	 * @deprecated An entity instance is not available if we are cancelling the mob spawn using the {@link com.destroystokyo.paper.event.entity.PreCreatureSpawnEvent}. This method will
+	 * attempt to create a default entity when it is not available, but you should use {@link #getEntityOrNull()} instead.
+	 */
+	@Deprecated(forRemoval = true, since = "0.103.0.5")
+	public @NonNull Entity getEntity() {
+		if (this.entity == null) {
+			return location.getWorld().createEntity(location, Objects.requireNonNull(entityType.getEntityClass(), "entity class"));
+		}
+
+		return this.entity;
 	}
 
-	public static HandlerList getHandlerList() {
-		return handlers;
+	/**
+	 * {@return the entity being attempted to be spawned, or null}
+	 */
+	public @Nullable Entity getEntityOrNull() {
+		return this.entity;
+	}
+
+	/**
+	 * {@return the type of the entity being attempted to be spawned}
+	 */
+	public EntityType getEntityType() {
+		return this.entityType;
+	}
+
+	/**
+	 * {@return the location at which the entity is being attempted to be spawned}
+	 */
+	public Location getLocation() {
+		return this.location.clone();
+	}
+
+	/**
+	 * {@return the reason for the entity being spawned}
+	 */
+	public CreatureSpawnEvent.SpawnReason getSpawnReason() {
+		return spawnReason;
 	}
 
 	@Override
@@ -37,12 +85,12 @@ public class MobSpawnRemovalEvent extends Event implements Cancellable {
 		cancelled = isCancelled;
 	}
 
-	public Entity getEntity() {
-		return this.entity;
+	@Override
+	public @NonNull HandlerList getHandlers() {
+		return HANDLER_LIST;
 	}
 
-	public EntityType getEntityType() {
-		return this.entity.getType();
+	public static HandlerList getHandlerList() {
+		return HANDLER_LIST;
 	}
-
 }
